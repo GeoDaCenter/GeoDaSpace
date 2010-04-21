2ae32762d4aceb680143c4626751076eb375aa60
lanselin <lanselin@a2f3893c-e248-4646-8fd3-e29ca8a5c454>
2010-04-21 17:38:40 +0000

    LA comments on ols.py to test the system

--- ols.py
+++ ols.py
@@ -6,15 +6,19 @@ class OLS:
     def __init__(self):
         pass
 
-
+# LA - should probably be get_olsbetas(x,y)
 def get_betas(x,y):
     xt=np.transpose(x)
     xx=np.dot(xt,x)
     ixx=la.inv(xx)
+    # LA - preferred is to compute xy = np.dot(xt,y) first
+    # LA - next betas = np.dot(ixx,xy)
     ixxx=np.dot(ixx,xt)
     betas=np.dot(ixxx,y)
+    # LA - in addition to betas, return ixx, used to compute var(betas)
     return betas
 
+# LA - note: residuals are generic for any linear model,
 def get_residuals(x,y,betas):
 	predy=np.dot(x,betas)
 	residuals=y-predy
