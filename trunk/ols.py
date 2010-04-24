5d795c01e2655f0ee8f52ce3b180a141bf3e8583
daarriba <daarriba@a2f3893c-e248-4646-8fd3-e29ca8a5c454>
2010-04-24 01:05:08 +0000

    Clean up and documentation of ols, added examples folder and tasks file

--- trunk/ols.py
+++ trunk/ols.py
@@ -3,29 +3,83 @@ import numpy.linalg as la
 
 
 class OLS:
+    """
+    OLS class for end-user (gives back only results ans diagnostics)
+    """
     def __init__(self):
         pass
 
-# LA - should probably be get_olsbetas(x,y)
-def get_betas(x,y):
-    xt=np.transpose(x)
-    xx=np.dot(xt,x)
-    ixx=la.inv(xx)
-    # LA - preferred is to compute xy = np.dot(xt,y) first
-    # LA - next betas = np.dot(ixx,xy)
-    ixxx=np.dot(ixx,xt)
-    betas=np.dot(ixxx,y)
-    # LA - in addition to betas, return ixx, used to compute var(betas)
-    return betas
+class OLS_dev:
+    """
+    OLS class to do all the computations
 
-# LA - note: residuals are generic for any linear model,
-def get_residuals(x,y,betas):
-	predy=np.dot(x,betas)
-	residuals=y-predy
-	# LA - in addition to residuals, return predicted values
-    return residuals
+    NOTE: no consistency checks
+
+    Parameters
+    ----------
+
+    x       : array
+              nxk array of independent variables (assumed to be aligned with y)
+    y       : array
+              nx1 array of dependent variable
+    constant: boolean
+              If true it appends a vector of ones to the independent variables
+              to estimate intercept (set to True by default)
+
+    Attributes
+    ----------
+
+    betas   : array
+              kx1 array with estimated coefficients
+    xt      : array
+              kxn array of transposed independent variables
+    xtx     : array
+              kxk array
+    xtxi    : array
+              kxk array of inverted xtx
+    u       : array
+              nx1 array of residuals
+    predy   : array
+              nx1 array of predicted values
+
+    Examples
+    --------
+
+    >>> import numpy as np
+    >>> import pysal
+    >>> db=pysal.open("examples/columbus.dbf","r")
+    >>> var_names=db.header
+    >>> data=np.array(db[:])
+    >>> y=data[:,var_names.index("CRIME")]
+    >>> X=data[:,[var_names.index(v) for v in ["INC","HOVAL"]]]
+    >>> ols=OLS_dev(X,y)
+    >>> ols.betas
+    array([ 68.6189611 ,  -1.59731083,  -0.27393148])
+    
+    """
+    def __init__(self,x,y,constant=True):
+        if constant:
+            x = np.hstack((np.ones(y.shape),x))
+        xt = np.transpose(x)
+        xtx = np.dot(xt,x)
+        xtxi = la.inv(xtx)
+        xty = np.dot(xt,y)
+        self.betas = np.dot(xtxi,xty)
+        self.xtxi = xtxi
+        self.xtx = xtx
+        self.xt = xt
+        predy = np.dot(x,betas)
+        u = y-predy
+        self.u = u
+        self.predy = predy
 
 
+def _test():
+    import doctest
+    doctest.testmod()
+
+if __name__ == '__main__':
+    _test()
 
 
 
