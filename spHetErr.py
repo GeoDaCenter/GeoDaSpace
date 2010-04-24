f965d79fed9b5d01fdf984a3ea19d27918a8a869
daarriba <daarriba@a2f3893c-e248-4646-8fd3-e29ca8a5c454>
2010-04-24 01:05:08 +0000

    Clean up and documentation of ols, added examples folder and tasks file

--- spHetErr.py
+++ spHetErr.py
@@ -8,31 +8,29 @@ class Spatial_Error_Het:
     def __init__(self,x,y,w):
 
         #1a. OLS --> \tilde{betas}
-        betas1=OLS.get_betas(x,y)
+        ols = OLS.OLS_dev(x,y)
 
         #1b. GMM --> \tilde{\lambda1}
-        residuals1=OLS.get_residuals(x,y,betas1)
-        moments1=GMM.Moments(residuals1,w)
-        lambda1=optimizer(moments1.moments)
+        moments1 = GMM.Moments(ols.u,w)
+        lambda1 = optimizer(moments1.moments)
 
         #1c. GMM --> \tilde{\lambda2}
-        vc1=get_vc(residuals1,w,lambda1,moments1)
-        lambda2=optimizer(moments1,vc1)
+        vc1 = get_vc(ols.u,w,lambda1,moments1)
+        lambda2 = optimizer(moments1,vc1)
 
         #2a. OLS -->\hat{betas}
-        xs,ys=get_spCO(x,w,lambda2),get_spCO(y,w,lambda2)
-        betas2=OLS.get_betas(xs,ys)
+        xs,ys = get_spCO(x,w,lambda2),get_spCO(y,w,lambda2)
+        ols = OLS.OLS_dev(xs,ys)
 
         #2b. GMM --> \hat{\lambda}
-        residuals2=OLS.get_residuals(xs,ys,betas2)
-        moments2=GMM.Moments(residuals2,w)
-        vc2=GMM.get_vc(residuals2,w,lambda2,moments2)
-        lambda3=optimizer(moments2.moments,vc2)
+        moments2 = GMM.Moments(ols.u,w)
+        vc2 = GMM.get_vc(ols.u,w,lambda2,moments2)
+        lambda3 = optimizer(moments2.moments,vc2)
 
         #Output
-        self.betas=betas2
-        self.lamb=lambda3
-        self.residuals=residuals2
+        self.betas = ols.betas
+        self.lamb = lambda3
+        self.u = ols.u
 
         
 # LA - note: as written, requires recomputation of spatial lag
---
+++ tasks.rst
@@ -0,0 +1,24 @@
+
+=====
+Tasks
+=====
+
+* Research optimizer and fill `optimizer` in `spHetErr.py`
+
+* Handling of vectors: nx1 Vs. nxNone arrays
+
+* Comparison of PySAL sparse weights + algebra tricks Vs. Scipy sparse + brute force
+
+* Call up Charlie regarding computation of d_i
+
+* Check step 2a. and estimation procedure (ols Vs. FGLS)
+
+* Set up test system
+
+* Creating example datasets (100, 10000, 100000, 1000000)
+
+* Write function for : Iteration of step 2.a and 2.b
+
+* Add to docstrings estimation of computational burden (O)
+
+* Cross-reference class notes with current code
