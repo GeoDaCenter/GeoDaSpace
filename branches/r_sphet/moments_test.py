acbefc067e2f7caa5f065ddf3da8239e829a0298
daarriba <daarriba@a2f3893c-e248-4646-8fd3-e29ca8a5c454>
2010-05-06 02:06:45 +0000

    -'spHetErr.py': changes in get_A1() for faster running -'moments_test.py': changes to allow to run n=1000000

--- branches/r_sphet/moments_test.py
+++ branches/r_sphet/moments_test.py
@@ -15,8 +15,9 @@ vars = {100:'n100_stdnorm_vars6.csv', 10000:'n10000_stdnorm_vars6.csv', 1000000:
 ks = ['small', 'medium', 'large']
 #ks = ['medium']
 
+"""
 fo = open('moments_runPy.txt', 'w')
-fo.write('Time elapsed to run gmmS.Moments in different datasets\n')
+fo.write('Time elapsed (secs) to run gmmS.Moments in different datasets\n')
 for n in ns:
     print 'N = %i'%n
     for k in ks:
@@ -29,12 +30,49 @@ for n in ns:
         w.S = get_S(w)
         w.A1 = get_A1(w.S) 
 
-        print 'Running Moments...'
+        print '   Running Moments...'
         t0 = time()
         m = Moments(w, u)
         t1 = time()
 
         line = '%s,%i,%f\n'%(k, n, t1-t0)
         fo.write(line)
+
+# 'small', n = 1000000
+"""
+n, k = 1000000, 'small'
+print 'N = %i'%n
+print '  k = %s'%k
+ti0 = time()
+data = Test_Data(n=n, k=k, folder=dataF)
+ti1 = time()
+print '   w loaded: %f seconds'%(ti1-ti0)
+u = pysal.open(dataF+'examples/'+vars[n])
+u = np.array([u.by_col('varA')]).T
+ti2 = time()
+print '   u created: %f'%(ti2-ti1)
+w = data.w
+w.transform = 'r'
+ti3 = time()
+print '   w transformed: %f seconds'%(ti3-ti2)
+w.S = get_S(w)
+ti4 = time()
+print '   w to sparse: %f seconds'%(ti4-ti3)
+w.A1 = get_A1(w.S) 
+ti5 = time()
+print '   w has A1: %f seconds'%(ti5-ti4)
+
+print '   Running Moments...'
+t0 = time()
+m = Moments(w, u)
+t1 = time()
+print 'Moments in %f seconds'%(t1-t0)
+
+"""
+line = '%s,%i,%f\n'%(k, n, t1-t0)
+fo.append(line)
+
+
 fo.close()
+"""
 
