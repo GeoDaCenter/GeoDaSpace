1e9395671d9af577f5ddd366fd3cfbb325cd91f0
daarriba <daarriba@a2f3893c-e248-4646-8fd3-e29ca8a5c454>
2010-05-10 15:58:48 +0000

    Minor fixes in Spatial_Error_Het. It  runs and gives numbers back. Now we have to test it gives the correct ones -Documentation added to Spatial_Error_Het

--- spHetErr.py
+++ spHetErr.py
@@ -8,28 +8,51 @@ from scipy import sparse as SP
 import time
 
 class Spatial_Error_Het:
-    """GMM method for a spatial error model with heteroskedasticity"""
-    def __init__(self,x,y,w,i=1): ######Inserted i parameter here for iterations...
+    """
+    GMM method for a spatial error model with heteroskedasticity
 
-        if not w.S:
-            w.S = get_S(w)
-        if not w.A1:
-            w.A1 = get_A1(w.S)
-        w.S = get_S(w)
-        w.A1 = get_A1(w.S)
+    NOTE: w is assumed to have w.S and w.A1
+    ...
+
+    Parameters
+    ----------
+
+    x           : array
+                  nxk array with independent variables aligned with y
+    y           : array
+                  nx1 array with dependent variables
+    w           : W
+                  PySAL weights instance aligned with y and with instances S
+                  and A1 created
+    cycles      : int
+                  Optional. Number of iterations of steps 2a. and 2b. Set to 1
+                  by default
+
+    Attributes
+    ----------
+    
+    betas       : array
+                  (k+1)x1 array with beta estimates for intercept and x
+    lamb        : float
+                  Estimate of lambda
+    u           : array
+                  nx1 array with residuals
+
+    """
+    def __init__(self,x,y,w,cycles=1): ######Inserted i parameter here for iterations...
 
         #1a. OLS --> \tilde{betas}
         ols = OLS.OLS_dev(x,y)
 
         #1b. GMM --> \tilde{\lambda1}
-        moments1 = GMM.Moments(ols.u,w)
-        lambda1 = Optimizer(moments1).lambdaX
+        moments = GMM.Moments(w, ols.u)
+        lambda1 = optimizer(moments)[0][0]
 
         #1c. GMM --> \tilde{\lambda2}
-        vc1 = get_vc(ols.u,w,lambda1,moments1)
-        lambda2 = Optimizer(moments1,vc1).lambdaX
+        vc1 = GMM.get_vc(w, ols.u, lambda1)
+        lambda2 = optimizer(moments,vc1)[0][0]
         
-        for n in range(i): #### Added loop.
+        for n in range(cycles): #### Added loop.
             #2a. OLS -->\hat{betas}
             xs,ys = get_spCO(x,w,lambda2),get_spCO(y,w,lambda2)
             
@@ -41,9 +64,9 @@ class Spatial_Error_Het:
             ols = OLS.OLS_dev(xs,ys)
 
             #2b. GMM --> \hat{\lambda}
-            moments2 = GMM.Moments(ols.u,w)
-            vc2 = GMM.get_vc(ols.u,w,lambda2,moments2)
-            lambda3 = Optimizer(moments2,vc2).lambdaX
+            moments = GMM.Moments(w, ols.u)
+            vc2 = GMM.get_vc(w, ols.u, lambda2)
+            lambda3 = optimizer(moments,vc2)[0][0]
             lambda2 = lambda3 #### 
 
             #How many times do we want to iterate after 2b.? What should value of i be
@@ -131,7 +154,7 @@ def get_A1(S):
     d = d.asformat('csr')
     return StS - d
 
-def optimizer(moments, vcX=None):
+def optimizer(moments, vcX=np.array([0])):
     """
     Optimization of moments
     ...
@@ -143,7 +166,8 @@ def optimizer(moments, vcX=None):
                   Instance of gmmS.Moments with G and g
     vcX         : array
                   Optional. 2x2 array with the Variance-Covariance matrix to be used as
-                  weights in the optimization (applies Cholesky decomposition). Set to None by default.
+                  weights in the optimization (applies Cholesky
+                  decomposition). Set empty by default.
 
     Returns
     -------
@@ -158,10 +182,10 @@ def optimizer(moments, vcX=None):
                         d['grad'] is the gradient at the minimum (should be 0 ish)
                         d['funcalls'] is the number of function calls made
     """
-    if vcX:
+    if vcX.any():
         Ec = np.transpose(la.cholesky(la.inv(vcX)))
-        self.moments_op.G = np.dot(Ec,moments_op.G)
-        self.moments_op.g = np.dot(Ec,moments_op.g)
+        moments.G = np.dot(Ec,moments.G)
+        moments.g = np.dot(Ec,moments.g)
         
     lambdaX = op.fmin_l_bfgs_b(kpgm,[0.0],args=[moments],approx_grad=True,bounds=[(-1.0,1.0)])
     return lambdaX
@@ -191,4 +215,13 @@ def kpgm(lambdapar,moments):
     vv2=vv-moments.g
     return sum(sum(vv2*vv2))
 
+if __name__ == '__main__':
+
+    from testing_utils import Test_Data as DAT
+    data = DAT()
+    y, x, w = data.y, data.x, data.w
+    y = np.array([y]).T
+    w.S = get_S(w)
+    w.A1 = get_A1(w.S)
+    sp = Spatial_Error_Het(x, y, w)
 
