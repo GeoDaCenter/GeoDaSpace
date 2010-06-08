592823b5b9c090350d1c5ee8912f910da53baca1
david21 <david21@a2f3893c-e248-4646-8fd3-e29ca8a5c454>
2010-06-08 17:18:19 +0000

    refactor of twosls, results match R

--- trunk/econometrics/twosls.py
+++ trunk/econometrics/twosls.py
@@ -2,11 +2,13 @@ import numpy as np
 import numpy.linalg as la
 import ols as OLS
 
-class TwoSLS:
+class TwoSLS(OLS.OLS):
     """
     2SLS class for end-user (gives back only results and diagnostics)
     """
     def __init__(self):
+
+
 
         # Need to check the shape of user input arrays, and report back descriptive
         # errors when necessary
@@ -29,8 +31,9 @@ class TwoSLS_dev(OLS.OLS_dev):
     y           : array
                   nx1 array of dependent variable
     h           : array
-                  nxl array of instruments
-    constants   : boolean
+                  nxl array of instruments; typically this includes all
+                  exogenous variables from x and instruments
+    constant    : boolean
                   If true it appends a vector of ones to the independent variables
                   to estimate intercept (set to True by default)
 
@@ -53,10 +56,6 @@ class TwoSLS_dev(OLS.OLS_dev):
                   kxk array of inverted xtx
     u           : array
                   nx1 array of residuals (based on original x matrix)
-    u_ols       : array
-                  nx1 array of residuals (based on transformed x matrix)
-    predy_ols   : array
-                  nx1 array of predicted values (based on transformed x matrix)
     predy       : array
                   nx1 array of predicted values (based on original x matrix)
     n           : int
@@ -69,16 +68,6 @@ class TwoSLS_dev(OLS.OLS_dev):
                   Sigma squared with n in the denominator
     sig2n_k     : float
                   Sigma squared with n-k in the denominator
-
-              .. math::
-                
-                    \sigma^2 = \dfrac{\tilde{u}' \tilde{u}}{N}
-    m       : array
-              Matrix M
-
-              .. math::
-
-                    M = I - X(X'X)^{-1}X'
 
 
     Examples
@@ -93,45 +82,40 @@ class TwoSLS_dev(OLS.OLS_dev):
     >>> X.append(db.by_col("INC"))
     >>> X.append(db.by_col("HOVAL"))
     >>> X = np.array(X).T
-    >>> h = db.by_col("DISCBD")
-    >>> h = db.by_col("PERIMETER")
-    >>> h = np.array(h)
-    >>> h = np.reshape(h, (49,1))
+    >>> # instrument for HOVAL with DISCBD
+    >>> h = []
+    >>> h.append(db.by_col("INC"))
+    >>> h.append(db.by_col("DISCBD"))
+    >>> h = np.array(h).T
     >>> reg=TwoSLS_dev(X,y,h)
     >>> reg.betas
-    array([[ 68.6189611 ],
-           [ -1.59731083],
-           [ -0.27393148]])
+    array([[ 88.46579584],
+           [  0.5200379 ],
+           [ -1.58216593]])
     
     """
     def __init__(self, x, y, h, constant=True):
         if constant:
             x = np.hstack((np.ones(y.shape),x))
+            z = np.hstack((np.ones(y.shape),h))
         # x_hat = Z(Z'Z)^-1 Z'X
-        z = np.hstack((x, h))
-        print z
-        print '***********************************************'
-        zpz = np.dot(z.T,z)   # k+l x k+l 
-        print zpz
-        print '***********************************************'
-        zpzi = la.inv(zpz)   # k+l x k+l 
-        print zpzi
-        print '***********************************************'
-        zpx = np.dot(z.T, x)           # k+l x k
-        print zpx
-        print '***********************************************'
-        zpzi_zpx = np.dot(zpzi, zpx)   # k+l x k
-        print zpzi_zpx
-        print '***********************************************'
-        x_hat = np.dot(z, zpzi_zpx)    #   n x k
+        zpz = np.dot(z.T,z)
+        zpzi = la.inv(zpz)
+        zpx = np.dot(z.T, x)
+        zpzi_zpx = np.dot(zpzi, zpx)
+        x_hat = np.dot(z, zpzi_zpx)
 
         OLS.OLS_dev.__init__(self, x_hat, y, constant=False)
-        self.predy_ols = self.predy         # using transformed data
-        self.u_ols = self.u                 # using transformed data
-        self.predy = np.dot(x,self.betas)   # using original data
-        self.u = y - self.predy               # using original data
+        self.predy = np.dot(x, self.betas)   # using original data
+        self.u = y - self.predy             # using original data
         self.x = x
         self.h = h
+
+        # Currently self.xtxi, self.xtx, and self.xt are still based on the
+        # transformed information matrix passed into OLS_dev. The OLS_dev
+        # properties also need to be checked for appropriatness for 2SLS.
+        # Currently, self.vm matches R output and dignostics.stdError_Betas
+        # matches R output.
 
 
 def _test():
