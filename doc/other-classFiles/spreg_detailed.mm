e9aab26ab65d8ccb30df5353684fd620c305bb12
eamack1 <eamack1@a2f3893c-e248-4646-8fd3-e29ca8a5c454>
2010-06-08 16:06:56 +0000

    Mind map of spreg

---
+++ doc/other-classFiles/spreg_detailed.mm
@@ -0,0 +1,283 @@
+<map version="0.8.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1275585472581" ID="Freemind_Link_1621692918" MODIFIED="1275585472581" TEXT="spreg_detailed">
+<font BOLD="true" NAME="Dialog" SIZE="12"/>
+<node COLOR="#000000" CREATED="1275585472582" ID="Freemind_Link_1611416979" MODIFIED="1275585472582" POSITION="left" TEXT="Weights">
+<font BOLD="true" NAME="Dialog" SIZE="12"/>
+<hook NAME="accessories/plugins/NodeNote.properties">
+<text>Weights need to be treated differently from data, because weights are used to transform data</text>
+</hook>
+<node COLOR="#000000" CREATED="1275585472584" MODIFIED="1275585472584" TEXT="Model weights">
+<font BOLD="true" NAME="Dialog" SIZE="12"/>
+<hook NAME="accessories/plugins/NodeNote.properties">
+<text>for basic model specification, only one weights specification allowed;although keep option open for higher order lags (biparametric)</text>
+</hook>
+</node>
+<node COLOR="#000000" CREATED="1275585472584" MODIFIED="1275585472584" TEXT="Diagnostic Weights">
+<font BOLD="true" NAME="Dialog" SIZE="12"/>
+<hook NAME="accessories/plugins/NodeNote.properties">
+<text>weights to be used in diagnostic tests, can be multiple</text>
+</hook>
+</node>
+<node COLOR="#000000" CREATED="1275585472585" MODIFIED="1275585472585" TEXT="Kernel">
+<font BOLD="true" NAME="Dialog" SIZE="12"/>
+</node>
+</node>
+<node COLOR="#000000" CREATED="1275585472585" MODIFIED="1275585472585" POSITION="right" TEXT="Stats and Tests">
+<font BOLD="true" NAME="Dialog" SIZE="12"/>
+<node COLOR="#000000" CREATED="1275585472585" MODIFIED="1275585472585" TEXT="LM">
+<font BOLD="true" NAME="Dialog" SIZE="12"/>
+<icon BUILTIN="button_ok"/>
+</node>
+<node COLOR="#000000" CREATED="1275585472585" MODIFIED="1275585472585" TEXT="Robust LM">
+<font BOLD="true" NAME="Dialog" SIZE="12"/>
+</node>
+<node COLOR="#000000" CREATED="1275585472585" MODIFIED="1275585472585" TEXT="AK">
+<font BOLD="true" NAME="Dialog" SIZE="12"/>
+</node>
+<node COLOR="#000000" CREATED="1275585472585" MODIFIED="1275585472585" TEXT="AIC">
+<font BOLD="true" NAME="Dialog" SIZE="12"/>
+</node>
+<node COLOR="#000000" CREATED="1275585472585" MODIFIED="1275585472585" TEXT="BIC">
+<font BOLD="true" NAME="Dialog" SIZE="12"/>
+</node>
+<node COLOR="#000000" CREATED="1275585472585" MODIFIED="1275585472585" TEXT="SIC">
+<font BOLD="true" NAME="Dialog" SIZE="12"/>
+</node>
+<node COLOR="#000000" CREATED="1275585472585" MODIFIED="1275585472585" TEXT="Jarque-Bera">
+<font BOLD="true" NAME="Dialog" SIZE="12"/>
+</node>
+<node COLOR="#000000" CREATED="1275585472586" MODIFIED="1275585472586" TEXT="R^2">
+<font BOLD="true" NAME="Dialog" SIZE="12"/>
+</node>
+<node COLOR="#000000" CREATED="1275585472586" MODIFIED="1275585472586" TEXT="Adj R^2">
+<font BOLD="true" NAME="Dialog" SIZE="12"/>
+</node>
+<node COLOR="#000000" CREATED="1275585472586" MODIFIED="1275585472586" TEXT="Sargan">
+<font BOLD="true" NAME="Dialog" SIZE="12"/>
+</node>
+<node COLOR="#000000" CREATED="1275585472586" MODIFIED="1275585472586" TEXT="Hausman">
+<font BOLD="true" NAME="Dialog" SIZE="12"/>
+</node>
+<node COLOR="#000000" CREATED="1275585472586" MODIFIED="1275585472586" TEXT="Chi^2">
+<font BOLD="true" NAME="Dialog" SIZE="12"/>
+</node>
+<node COLOR="#000000" CREATED="1275585472586" MODIFIED="1275585472586" TEXT="t-tests">
+<font BOLD="true" NAME="Dialog" SIZE="12"/>
+</node>
+<node COLOR="#000000" CREATED="1275585472587" MODIFIED="1275585472587" TEXT="Likelihood">
+<font BOLD="true" NAME="Dialog" SIZE="12"/>
+</node>
+<node COLOR="#000000" CREATED="1275585472587" MODIFIED="1275585472587" TEXT="Chow">
+<font BOLD="true" NAME="Dialog" SIZE="12"/>
+</node>
+<node COLOR="#000000" CREATED="1275585472587" MODIFIED="1275585472587" TEXT="Wald">
+<font BOLD="true" NAME="Dialog" SIZE="12"/>
+</node>
+<node COLOR="#000000" CREATED="1275585472587" MODIFIED="1275585472587" TEXT="Breusch-Pagan">
+<font BOLD="true" NAME="Dialog" SIZE="12"/>
+</node>
+<node COLOR="#000000" CREATED="1275585472587" MODIFIED="1275585472587" TEXT="Koenker-Bassett">
+<font BOLD="true" NAME="Dialog" SIZE="12"/>
+</node>
+<node COLOR="#000000" CREATED="1275585472587" ID="Freemind_Link_1646941960" MODIFIED="1275585498653" TEXT="White">
+<font BOLD="true" NAME="Dialog" SIZE="12"/>
+</node>
+</node>
+<node COLOR="#000000" CREATED="1275585472588" ID="Freemind_Link_822815563" MODIFIED="1275585472588" POSITION="right" TEXT="Estimation methods users can call">
+<font BOLD="true" NAME="Dialog" SIZE="12"/>
+<node COLOR="#000000" CREATED="1275585472588" ID="Freemind_Link_1740574798" MODIFIED="1275585472588" TEXT="Classics">
+<font BOLD="true" NAME="Dialog" SIZE="12"/>
+<node COLOR="#000000" CREATED="1275585472588" ID="Freemind_Link_981139648" MODIFIED="1275585472588" TEXT="OLS">
+<font BOLD="true" NAME="Dialog" SIZE="12"/>
+<node COLOR="#000000" CREATED="1275585472588" MODIFIED="1275585472588" TEXT="Standard">
+<font BOLD="true" NAME="Dialog" SIZE="12"/>
+</node>
+<node COLOR="#000000" CREATED="1275585472589" MODIFIED="1275585472589" TEXT="White">
+<font BOLD="true" NAME="Dialog" SIZE="12"/>
+</node>
+</node>
+<node COLOR="#000000" CREATED="1275585472589" ID="Freemind_Link_557177928" MODIFIED="1275585472589" TEXT="2SLS">
+<font BOLD="true" NAME="Dialog" SIZE="12"/>
+<node COLOR="#000000" CREATED="1275585472589" ID="Freemind_Link_1042495510" MODIFIED="1275585472589" TEXT="Standard">
+<font BOLD="true" NAME="Dialog" SIZE="12"/>
+<node COLOR="#000000" CREATED="1275585472589" MODIFIED="1275585472589" TEXT="OLS - Predicted Y">
+<font BOLD="true" NAME="Dialog" SIZE="12"/>
+</node>
+<node COLOR="#000000" CREATED="1275585472589" MODIFIED="1275585472589" TEXT="OLS - Standard">
+<font BOLD="true" NAME="Dialog" SIZE="12"/>
+</node>
+</node>
+<node COLOR="#000000" CREATED="1275585472589" MODIFIED="1275585472589" TEXT="Robust">
+<font BOLD="true" NAME="Dialog" SIZE="12"/>
+</node>
+</node>
+<node COLOR="#000000" CREATED="1275585472589" ID="Freemind_Link_1197842675" MODIFIED="1275585472589" TEXT="SUR">
+<font BOLD="true" NAME="Dialog" SIZE="12"/>
+<node COLOR="#000000" CREATED="1275585472589" MODIFIED="1275585472589" TEXT="ML">
+<font BOLD="true" NAME="Dialog" SIZE="12"/>
+</node>
+<node COLOR="#000000" CREATED="1275585472589" MODIFIED="1275585472589" TEXT="IV">
+<font BOLD="true" NAME="Dialog" SIZE="12"/>
+</node>
+</node>
+</node>
+<node COLOR="#000000" CREATED="1275585472589" ID="Freemind_Link_9188443" MODIFIED="1275585472589" TEXT="Spatial Lag">
+<font BOLD="true" NAME="Dialog" SIZE="12"/>
+<node COLOR="#000000" CREATED="1275585472589" ID="Freemind_Link_460555745" MODIFIED="1275585472589" TEXT="Panel">
+<font BOLD="true" NAME="Dialog" SIZE="12"/>
+<node COLOR="#000000" CREATED="1275585472589" MODIFIED="1275585472589" TEXT="ML">
+<font BOLD="true" NAME="Dialog" SIZE="12"/>
+</node>
+<node COLOR="#000000" CREATED="1275585472589" MODIFIED="1275585472589" TEXT="GMM">
+<font BOLD="true" NAME="Dialog" SIZE="12"/>
+</node>
+</node>
+<node COLOR="#000000" CREATED="1275585472589" ID="Freemind_Link_1312363065" MODIFIED="1275585472589" TEXT="Cross-sectional">
+<font BOLD="true" NAME="Dialog" SIZE="12"/>
+<node COLOR="#000000" CREATED="1275585472590" MODIFIED="1275585472590" TEXT="ML">
+<font BOLD="true" NAME="Dialog" SIZE="12"/>
+</node>
+<node COLOR="#000000" CREATED="1275585472590" MODIFIED="1275585472590" TEXT="GMM">
+<font BOLD="true" NAME="Dialog" SIZE="12"/>
+</node>
+<node COLOR="#000000" CREATED="1275585472590" MODIFIED="1275585472590" TEXT="HAC">
+<font BOLD="true" NAME="Dialog" SIZE="12"/>
+</node>
+</node>
+<node COLOR="#000000" CREATED="1275585472590" ID="Freemind_Link_898612011" MODIFIED="1275585472590" TEXT="SUR">
+<font BOLD="true" NAME="Dialog" SIZE="12"/>
+<node COLOR="#000000" CREATED="1275585472590" MODIFIED="1275585472590" TEXT="ML">
+<font BOLD="true" NAME="Dialog" SIZE="12"/>
+</node>
+<node COLOR="#000000" CREATED="1275585472590" ID="Freemind_Link_50876421" MODIFIED="1275585472590" TEXT="IV">
+<font BOLD="true" NAME="Dialog" SIZE="12"/>
+<node COLOR="#000000" CREATED="1275585472590" MODIFIED="1275585472590" TEXT="Cross-Section Loader">
+<font BOLD="true" NAME="Dialog" SIZE="12"/>
+</node>
+<node COLOR="#000000" CREATED="1275585472590" MODIFIED="1275585472590" TEXT="2sls - residuals">
+<font BOLD="true" NAME="Dialog" SIZE="12"/>
+</node>
+<node COLOR="#000000" CREATED="1275585472590" MODIFIED="1275585472590" TEXT="Sigmas matrix (SUR only)">
+<font BOLD="true" NAME="Dialog" SIZE="12"/>
+</node>
+<node COLOR="#ff0000" CREATED="1275585472590" MODIFIED="1275585472590" TEXT="Stacked Matrices Multiplications">
+<font BOLD="true" NAME="Dialog" SIZE="12"/>
+</node>
+<node COLOR="#000000" CREATED="1275585472590" MODIFIED="1275585472590" TEXT="Residuals correlation">
+<font BOLD="true" NAME="Dialog" SIZE="12"/>
+</node>
+</node>
+</node>
+</node>
+<node COLOR="#000000" CREATED="1275585472590" ID="Freemind_Link_1807666665" MODIFIED="1275585472590" TEXT="Spatial Error">
+<font BOLD="true" NAME="Dialog" SIZE="12"/>
+<node COLOR="#000000" CREATED="1275585472590" ID="Freemind_Link_987165246" MODIFIED="1275585472590" TEXT="SUR">
+<font BOLD="true" NAME="Dialog" SIZE="12"/>
+<node COLOR="#000000" CREATED="1275585472590" MODIFIED="1275585472590" TEXT="ML">
+<font BOLD="true" NAME="Dialog" SIZE="12"/>
+</node>
+<node COLOR="#000000" CREATED="1275585472591" MODIFIED="1275585472591" TEXT="IV">
+<font BOLD="true" NAME="Dialog" SIZE="12"/>
+</node>
+</node>
+<node COLOR="#000000" CREATED="1275585472591" ID="Freemind_Link_787893276" MODIFIED="1275585472591" TEXT="Cross-sectional">
+<font BOLD="true" NAME="Dialog" SIZE="12"/>
+<node COLOR="#000000" CREATED="1275585472591" MODIFIED="1275585472591" TEXT="ML">
+<font BOLD="true" NAME="Dialog" SIZE="12"/>
+</node>
+<node COLOR="#000000" CREATED="1275585472591" MODIFIED="1275585472591" TEXT="GMM">
+<font BOLD="true" NAME="Dialog" SIZE="12"/>
+</node>
+<node COLOR="#000000" CREATED="1275585472591" MODIFIED="1275585472591" TEXT="KP HET">
+<font BOLD="true" NAME="Dialog" SIZE="12"/>
+</node>
+</node>
+<node COLOR="#000000" CREATED="1275585472591" ID="Freemind_Link_1508273958" MODIFIED="1275585472591" TEXT="Panel">
+<font BOLD="true" NAME="Dialog" SIZE="12"/>
+<node COLOR="#000000" CREATED="1275585472591" MODIFIED="1275585472591" TEXT="ML">
+<font BOLD="true" NAME="Dialog" SIZE="12"/>
+</node>
+<node COLOR="#000000" CREATED="1275585472591" ID="Freemind_Link_478828753" MODIFIED="1275585472591" TEXT="GMM">
+<font BOLD="true" NAME="Dialog" SIZE="12"/>
+<node COLOR="#000000" CREATED="1275585472591" MODIFIED="1275585472591" TEXT="OLS - Residuals">
+<font BOLD="true" NAME="Dialog" SIZE="12"/>
+</node>
+<node COLOR="#ff0000" CREATED="1275585472591" ID="Freemind_Link_1673300310" MODIFIED="1275585472591" TEXT="KP - wuwu">
+<font BOLD="true" NAME="Dialog" SIZE="12"/>
+<node COLOR="#ff0000" CREATED="1275585472591" MODIFIED="1275585472591" TEXT="First">
+<font BOLD="true" NAME="Dialog" SIZE="12"/>
+</node>
+<node COLOR="#ff0000" CREATED="1275585472591" MODIFIED="1275585472591" TEXT="Second">
+<font BOLD="true" NAME="Dialog" SIZE="12"/>
+</node>
+</node>
+<node COLOR="#000000" CREATED="1275585472591" MODIFIED="1275585472591" TEXT="Non-Linear Optimizer (need to find good python library - curvefitting from scipy?)">
+<font BOLD="true" NAME="Dialog" SIZE="12"/>
+</node>
+<node COLOR="#000000" CREATED="1275585472591" MODIFIED="1275585472591" TEXT="SPWLS">
+<font BOLD="true" NAME="Dialog" SIZE="12"/>
+</node>
+</node>
+</node>
+</node>
+<node COLOR="#000000" CREATED="1275585472591" ID="Freemind_Link_1384277788" MODIFIED="1275585472591" TEXT="Spatial Lag + Error">
+<font BOLD="true" NAME="Dialog" SIZE="12"/>
+<node COLOR="#000000" CREATED="1275585472591" ID="Freemind_Link_1092370112" MODIFIED="1275585472591" TEXT="Panel">
+<font BOLD="true" NAME="Dialog" SIZE="12"/>
+<node COLOR="#000000" CREATED="1275585472592" MODIFIED="1275585472592" TEXT="ML">
+<font BOLD="true" NAME="Dialog" SIZE="12"/>
+</node>
+<node COLOR="#000000" CREATED="1275585472592" MODIFIED="1275585472592" TEXT="GMM">
+<font BOLD="true" NAME="Dialog" SIZE="12"/>
+</node>
+</node>
+<node COLOR="#000000" CREATED="1275585472592" ID="Freemind_Link_1465155703" MODIFIED="1275585472592" TEXT="Cross-sectional">
+<font BOLD="true" NAME="Dialog" SIZE="12"/>
+<node COLOR="#000000" CREATED="1275585472592" MODIFIED="1275585472592" TEXT="ML">
+<font BOLD="true" NAME="Dialog" SIZE="12"/>
+</node>
+<node COLOR="#000000" CREATED="1275585472592" MODIFIED="1275585472592" TEXT="GMM">
+<font BOLD="true" NAME="Dialog" SIZE="12"/>
+</node>
+<node COLOR="#000000" CREATED="1275585472592" MODIFIED="1275585472592" TEXT="KP HET">
+<font BOLD="true" NAME="Dialog" SIZE="12"/>
+</node>
+</node>
+<node COLOR="#000000" CREATED="1275585472592" ID="Freemind_Link_972233180" MODIFIED="1275585472592" TEXT="SUR">
+<font BOLD="true" NAME="Dialog" SIZE="12"/>
+<node COLOR="#000000" CREATED="1275585472592" MODIFIED="1275585472592" TEXT="ML">
+<font BOLD="true" NAME="Dialog" SIZE="12"/>
+</node>
+<node COLOR="#000000" CREATED="1275585472592" MODIFIED="1275585472592" TEXT="IV">
+<font BOLD="true" NAME="Dialog" SIZE="12"/>
+</node>
+</node>
+</node>
+</node>
+<node COLOR="#000000" CREATED="1275585472582" MODIFIED="1275585472582" POSITION="left" TEXT="Data" VSHIFT="810">
+<font BOLD="true" NAME="Dialog" SIZE="12"/>
+<node COLOR="#000000" CREATED="1275585472582" MODIFIED="1275585472582" TEXT="Y">
+<font BOLD="true" NAME="Dialog" SIZE="12"/>
+</node>
+<node COLOR="#000000" CREATED="1275585472582" MODIFIED="1275585472582" TEXT="X">
+<font BOLD="true" NAME="Dialog" SIZE="12"/>
+</node>
+<node COLOR="#000000" CREATED="1275585472582" MODIFIED="1275585472582" TEXT="H">
+<font BOLD="true" NAME="Dialog" SIZE="12"/>
+</node>
+<node COLOR="#000000" CREATED="1275585472582" MODIFIED="1275585472582" TEXT="YE">
+<font BOLD="true" NAME="Dialog" SIZE="12"/>
+</node>
+<node COLOR="#000000" CREATED="1275585472582" MODIFIED="1275585472582" TEXT="Time">
+<font BOLD="true" NAME="Dialog" SIZE="12"/>
+</node>
+<node COLOR="#000000" CREATED="1275585472582" MODIFIED="1275585472582" TEXT="Space">
+<font BOLD="true" NAME="Dialog" SIZE="12"/>
+</node>
+<node COLOR="#000000" CREATED="1275585472582" MODIFIED="1275585472582" TEXT="Regimes">
+<font BOLD="true" NAME="Dialog" SIZE="12"/>
+</node>
+</node>
+</node>
+</map>
