b838128d9b0e8fe9b422db487187cf26e79ba20f
daarriba <daarriba@a2f3893c-e248-4646-8fd3-e29ca8a5c454>
2010-07-22 20:00:34 +0000

    * Re-factoring of GMM tools: old non-scipy-sparse go now to 'gmm_deprecated.py' * Created 'gmm_utils.py' to host all moment and optimization calculations. * Modification of spHetErr.py to comply with previous points * Created spErr.py to host spatial error models. * Mock-up of GSLS matching R's spdep. Needs some refinement (see ToDo in file).

---
+++ econometrics/gmm_utils.py
@@ -0,0 +1,332 @@
+"""
+Tools for different GMM procedure estimations
+"""
+
+import numpy as np
+import pylab as pl
+from scipy import sparse as SP
+import scipy.optimize as op
+import numpy.linalg as la
+
+def get_spCO(z,w,lambdaX):
+    """
+    Spatial Cochrane-Orcut Transf
+
+    ...
+
+    Parameters
+    ----------
+
+    Returns
+    -------
+
+    """
+    return z - lambdaX * (w.sparse * z)
+
+# Arraiz et al.
+
+def get_A1(S):
+    """
+    Builds A1 as in Arraiz et al [1]_
+
+    .. math::
+
+        A_1 = W' W - diag(w'_{.i} w_{.i})
+
+    ...
+
+    Parameters
+    ----------
+
+    S               : csr_matrix
+                      PySAL W object converted into Scipy sparse matrix
+
+    Returns
+    -------
+
+    Implicit        : csr_matrix
+                      A1 matrix in scipy sparse format
+    References
+    ----------
+
+    .. [1] Arraiz, I., Drukker, D. M., Kelejian, H., Prucha, I. R. (2010) "A
+    Spatial Cliff-Ord-Type Model with Heteroskedastic Innovations: Small and
+    Large Sample Results". Journal of Regional Science, Vol. 60, No. 2, pp.
+    592-614.
+
+           
+    """
+    StS = S.T * S
+    d = SP.spdiags([StS.diagonal()], [0], S.get_shape()[0], S.get_shape()[1])
+    d = d.asformat('csr')
+    return StS - d
+
+def moments_het(w, u):
+    """
+    Class to compute all six components of the system of equations for a
+    spatial error model with heteroskedasticity estimated by GMM as in Arraiz
+    et al [1]_
+
+    Scipy sparse matrix version. It implements eqs. A.1 in Appendix A of
+    Arraiz et al. (2007) by using all matrix manipulation
+    
+    [g1] + [G11 G12] *  [\lambda]    = [0]
+    [g2]   [G21 G22]    [\lambda^2]    [0]
+
+    NOTE: 'residuals' has been renamed 'u' to fit paper notation
+
+    ...
+    
+    Parameters
+    ----------
+
+    w           : W
+                  Spatial weights instance (requires 'S' and 'A1')
+
+    u           : array
+                  Residuals. nx1 array assumed to be aligned with w
+ 
+    Attributes
+    ----------
+
+    moments     : list
+                  List of two arrays corresponding to the matrices 'G' and
+                  'g', respectively.
+
+    References
+    ----------
+
+    .. [1] Arraiz, I., Drukker, D. M., Kelejian, H., Prucha, I. R. (2010) "A
+    Spatial Cliff-Ord-Type Model with Heteroskedastic Innovations: Small and
+    Large Sample Results". Journal of Regional Science, Vol. 60, No. 2, pp.
+    592-614.
+
+    """
+    ut = u.T
+    S = w.sparse
+    St = S.T
+
+    utSt = ut * St
+    A1u = w.A1 * u
+    Su = S * u
+
+    g1 = np.dot(ut, A1u)
+    g2 = np.dot(ut, Su)
+    g = np.array([[g1][0][0],[g2][0][0]]) / w.n
+
+    G11 = 2 * (np.dot(utSt, A1u)) 
+    G12 = -np.dot(utSt * w.A1, Su)
+    G21 = np.dot(utSt, ((S + St) * u))
+    G22 = -np.dot(utSt, (S * Su))
+    G = np.array([[G11[0][0],G12[0][0]],[G21[0][0],G22[0][0]]]) / w.n
+
+    return [G, g]
+
+def get_vc_het(w, u, l):
+    """
+    Computes the VC matrix Psi based on lambda as in Arraiz et al [1]_:
+
+    ..math::
+
+        \tilde{Psi} = \left(\begin{array}{c c}
+                            \psi_{11} & \psi_{12} \\
+                            \psi_{21} & \psi_{22} \\
+                      \end{array} \right)
+
+    NOTE: psi12=psi21
+
+    ...
+
+    Parameters
+    ----------
+
+    w           : W
+                  Spatial weights instance (requires 'S' and 'A1')
+
+    u           : array
+                  Residuals. nx1 array assumed to be aligned with w
+
+    l           : float
+                  Lambda parameter estimate
+ 
+    Returns
+    -------
+
+    Psi         : array
+                  2x2 array with estimator of the variance-covariance matrix
+
+    References
+    ----------
+
+    .. [1] Arraiz, I., Drukker, D. M., Kelejian, H., Prucha, I. R. (2010) "A
+    Spatial Cliff-Ord-Type Model with Heteroskedastic Innovations: Small and
+    Large Sample Results". Journal of Regional Science, Vol. 60, No. 2, pp.
+    592-614.
+
+    """
+    e = (u - l * (w.sparse * u)) ** 2
+    E = SP.lil_matrix(w.sparse.get_shape())
+    E.setdiag(e.flat)
+    E = E.asformat('csr')
+    A1t = w.A1.T
+    wt = w.sparse.T
+
+    aPatE = (w.A1 + A1t) * E
+    wPwtE = (w.sparse + wt) * E
+
+    psi11 = aPatE * aPatE
+    psi12 = aPatE * wPwtE
+    psi22 = wPwtE * wPwtE 
+    psi = map(np.sum, [psi11.diagonal(), psi12.diagonal(), psi22.diagonal()])
+    return np.array([[psi[0], psi[1]], [psi[1], psi[2]]]) / (2 * w.n)
+
+def optimizer_het(moments, vcX=np.array([0])):
+    """
+    Optimization of moments
+    ...
+
+    Parameters
+    ----------
+
+    moments     : Moments
+                  Instance of gmm_utils.moments_het with G and g
+    vcX         : array
+                  Optional. 2x2 array with the Variance-Covariance matrix to be used as
+                  weights in the optimization (applies Cholesky
+                  decomposition). Set empty by default.
+
+    Returns
+    -------
+    x, f, d     : tuple
+                  x -- position of the minimum
+                  f -- value of func at the minimum
+                  d -- dictionary of information from routine
+                        d['warnflag'] is
+                            0 if converged
+                            1 if too many function evaluations
+                            2 if stopped for another reason, given in d['task']
+                        d['grad'] is the gradient at the minimum (should be 0 ish)
+                        d['funcalls'] is the number of function calls made
+    """
+    if vcX.any():
+        Ec = np.transpose(la.cholesky(la.inv(vcX)))
+        moments[0] = np.dot(Ec,moments[0])
+        moments[1] = np.dot(Ec,moments[1])
+        
+    lambdaX = op.fmin_l_bfgs_b(gm_het,[0.0],args=[moments],approx_grad=True,bounds=[(-1.0,1.0)])
+    return lambdaX
+
+def gm_het(lambdapar,moments):
+    """ 
+    Preparation of moments for minimization as in Arraiz et al [1]_
+    ...
+
+    Parameters
+    ----------
+
+    lambdapar       : float
+                      Spatial autoregressive parameter
+    moments         : moments_het
+                      Instance of gmm_utils.Moments with G and g
+
+    Returns
+    -------
+
+    minimum         : float
+                      sum of square residuals (e) of the equation system 
+                      moments.g - moments.G * lambdapar = e
+
+    References
+    ----------
+
+    .. [1] Arraiz, I., Drukker, D. M., Kelejian, H., Prucha, I. R. (2010) "A
+    Spatial Cliff-Ord-Type Model with Heteroskedastic Innovations: Small and
+    Large Sample Results". Journal of Regional Science, Vol. 60, No. 2, pp.
+    592-614.
+
+
+    """
+    par=np.array([float(lambdapar[0]),float(lambdapar[0])**float(2)])
+    vv=np.inner(moments[0],par)
+    vv2=np.reshape(vv,[2,1])-moments[1]
+    return sum(vv2**2)
+
+# GSLS
+
+def momentsGSLS(w, u):
+
+    u2 = np.dot(u.T, u)
+    wu = w.sparse * u
+    uwu = np.dot(u.T, wu)
+    wu2 = np.dot(wu.T, wu)
+    wwu = w.sparse * wu
+    uwwu = np.dot(u.T, wwu)
+    wwu2 = np.dot(wwu.T, wwu)
+    wuwwu = np.dot(wu.T, wwu)
+    wtw = w.sparse.T * w.sparse
+    trWtW = np.sum(wtw.diagonal())
+
+    g = np.array([[u2[0][0], wu2[0][0], uwu[0][0]]]).T / w.n
+
+    G = np.array([[2 * uwu[0][0], -wu2[0][0], w.n], [2 * wuwwu[0][0], -wwu2[0][0], trWtW], [uwwu[0][0] + wu2[0][0], -wuwwu[0][0], 0.]]) / w.n
+
+    return [G, g]
+
+def optimizer_gsls(moments):
+    """
+    Optimization of moments
+    ...
+
+    Parameters
+    ----------
+
+    moments     : Moments
+                  Instance of gmm_utils.Moments with G and g
+    vcX         : array
+                  Optional. 2x2 array with the Variance-Covariance matrix to be used as
+                  weights in the optimization (applies Cholesky
+                  decomposition). Set empty by default.
+
+    Returns
+    -------
+    x, f, d     : tuple
+                  x -- position of the minimum
+                  f -- value of func at the minimum
+                  d -- dictionary of information from routine
+                        d['warnflag'] is
+                            0 if converged
+                            1 if too many function evaluations
+                            2 if stopped for another reason, given in d['task']
+                        d['grad'] is the gradient at the minimum (should be 0 ish)
+                        d['funcalls'] is the number of function calls made
+    """
+       
+    lambdaX = op.fmin_l_bfgs_b(gm_gsls,[0.0, 0.0],args=[moments],approx_grad=True,bounds=[(-1.0,1.0), (0, None)])
+    return lambdaX
+
+def gm_gsls(lambdapar, moments):
+    """
+    Preparation of moments for minimization in a GSLS framework
+    """
+    par=np.array([[float(lambdapar[0]),float(lambdapar[0])**2., lambdapar[1]]]).T
+    vv = np.dot(moments[0], par)
+    vv = vv - moments[1]
+    return sum(vv**2)
+
+
+if __name__ == "__main__":
+
+    import random
+    import pysal
+    from spHetErr import get_A1
+    w=pysal.weights.lat2W(7,7, rook=False)
+    w.transform='r'
+    w.A1 = get_A1(w.sparse)
+    random.seed(100)
+    np.random.seed(100)
+    u=np.random.normal(0,1,(w.n,1))
+    u = np.random.randn(w.n,1) * (200*np.random.randn(w.n,1))
+
+    m=moments_het(w,u)
+    vc = get_vc_het(w, u, 0.1)
+
