491db781b99efdaec399045bf70713586d39b6e2
jing <jing@a2f3893c-e248-4646-8fd3-e29ca8a5c454>
2010-09-07 19:38:38 +0000

    add function get_spFilter() and delete the previous class

--- econometrics/gmm_utils.py
+++ econometrics/gmm_utils.py
@@ -8,6 +8,7 @@ from scipy import sparse as SP
 import scipy.optimize as op
 import numpy.linalg as la
 
+
 def get_spCO(z,w,lambdaX):
     """
     Spatial Cochrane-Orcut Transf
@@ -331,12 +332,130 @@ def gm_gsls(lambdapar, moments):
     vv = vv - moments[1]
     return sum(vv**2)
 
+# copy from spHetErr.py
+def get_S(w):
+    """
+    Converts pysal W to scipy csr_matrix
+    ...
+
+    Parameters
+    ----------
+
+    w               : W
+                     Spatial weights instance
+
+    Returns
+    -------
+
+    Implicit        : csr_matrix
+                      PySAL W object converted into Scipy sparse matrix
+                
+    """
+    data = []
+    indptr = [0]
+    indices = []
+    for ob in w.id_order:
+        data.extend(w.weights[ob])
+        indptr.append(indptr[-1] + len(w.weights[ob]))
+        indices.extend(w.neighbors[ob])
+    data = np.array(data)
+    indices = np.array(indices)
+    indptr = np.array(indptr)
+        
+    return SP.csr_matrix((data,indices,indptr),shape=(w.n,w.n))
+
+def get_spFilter(w,lamb,sf):
+    '''
+    computer the spatially filtered variables
+    
+    Parameters
+    ----------
+    w       : weight
+              PySAL weights instance  
+    lamda   : double
+              spatial autoregressive parameter
+    sp      : array
+              the variable needed to compute the filter
+    Returns
+    --------
+    rs      : array
+              spatially filtered variable
+    
+    Examples
+    --------
+
+    >>> import numpy as np
+    >>> import pysal
+    >>> db=pysal.open("examples/columbus.dbf","r")
+    >>> y = np.array(db.by_col("CRIME"))
+    >>> y = np.reshape(y, (49,1))
+    >>> w=pysal.open("examples/columbus.GAL").read()  
+    >>> solu = get_spFilter(X,y,w,0.5)
+    >>> print solu.ys 
+    >>>
+    [[ -15.7812905]
+    [ -32.158758 ]
+    [ -32.4865705]
+    [ -27.410798 ]
+    [ -97.3822125]
+    [  -3.9670885]
+    [ -91.2635635]
+    [ -99.43012  ]
+    [-118.107001 ]
+    [ -35.0421145]
+    [ -35.2276465]
+    [ -77.3782495]
+    [ -35.4055665]
+    [ -59.8451955]
+    [ -75.117567 ]
+    [-140.013057 ]
+    [ -33.4549225]
+    [ -29.5261305]
+    [ -27.6813485]
+    [-177.105114 ]
+    [ -19.001647 ]
+    [-119.702461 ]
+    [ -42.9538575]
+    [ -88.131421 ]
+    [-132.2616395]
+    [ -98.780399 ]
+    [ -19.629096 ]
+    [-118.43816  ]
+    [ -92.400958 ]
+    [ -60.3964295]
+    [ -31.2535   ]
+    [ -37.7388985]
+    [ -44.0567675]
+    [ -42.005636 ]
+    [ -69.722601 ]
+    [ -55.2186525]
+    [ -62.0116055]
+    [ -72.6139295]
+    [ -25.6059015]
+    [ -53.5011755]
+    [ -24.8541415]
+    [ -24.3181745]
+    [ -40.9453225]
+    [ -14.9928235]
+    [ -22.078858 ]
+    [ -12.8126545]
+    [  10.124343 ]
+    [  -9.115376 ]
+    [ -11.508765 ]]
+    '''        
+    # convert w into sparse matrix      
+    w_matrix = get_S(w)
+    rs = sf - lamb * w_matrix.dot(sf,)    
+    
+    return rs
+        
 
 if __name__ == "__main__":
-
+    
     import random
     import pysal
     from spHetErr import get_A1
+    
     w=pysal.weights.lat2W(7,7, rook=False)
     w.transform='r'
     w.A1 = get_A1(w.sparse)
@@ -347,4 +466,5 @@ if __name__ == "__main__":
 
     m=moments_het(w,u)
     vc = get_vc_het(w, u, 0.1)
+   
 
