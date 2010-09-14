90252c7edd0a39ceeba20428faadf5649c2628ec
dani <dani@a2f3893c-e248-4646-8fd3-e29ca8a5c454>
2010-09-14 09:17:25 +0000

    Startup for benchmarking spatial diagnostics module

---
+++ branches/benchmarking/python_bm/spDiag_bm.py
@@ -0,0 +1,17 @@
+"""
+Script to test the Spatial Diagnostics module on spreg
+"""
+
+trunk = '../../../trunk/econometrics/'
+
+# Data Loading
+from econometrics.testing_utils import Test_Data 
+
+## 10000 obs
+data = Test_Data(10000, 4, 'medium', trunk)
+
+# Moran's I
+
+# LM tests
+
+# AK tests
