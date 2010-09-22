06459d257666b0afa5b81b1ce6819f8c312d2244
phil <phil@a2f3893c-e248-4646-8fd3-e29ca8a5c454>
2010-09-22 02:17:42 +0000

    Removing call to intersphinx module, broke the build.

--- trunk/doc/source/conf.py
+++ trunk/doc/source/conf.py
@@ -23,7 +23,7 @@ sys.path.append('/Users/stephens/code/spreg/trunk/')
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest','sphinx.ext.intersphinx', 'sphinx.ext.autosummary', 'sphinx.ext.pngmath','numpydoc'] 
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.autosummary', 'sphinx.ext.pngmath','numpydoc'] 
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
