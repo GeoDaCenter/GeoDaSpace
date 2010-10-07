00e7681158db20d0e6fc42e0cb81fb7ab6443506
pedro <pedro@a2f3893c-e248-4646-8fd3-e29ca8a5c454>
2010-10-07 02:39:25 +0000

    probit code under development and tentative simulation code.

--- branches/probit_sim/multiProc_experiments.py
+++ branches/probit_sim/multiProc_experiments.py
@@ -56,15 +56,15 @@ if __name__ == '__main__':
     cycles_pc = 100000
     pool = mp.Pool(cores)
     t0 = time.time()
-    parts = pool.map(worker_task, [cycles_pc] * cores)
+    parts = pool.map(worker_task, [cycles_pc/cores] * cores)
     means = output_paster(parts)
     t1 = time.time()
     print '\nRun on multi-core: %.4f seconds'%(t1-t0)
     print 'Average value: ', np.mean(means)
-    single = worker_task(cycles_pc * cores)
+    single = worker_task((cycles_pc/cores) * cores)
     t2 = time.time()
     print '\nRun on single-core: %.4f seconds'%(t2-t1)
     print 'Average value: ', np.mean(single)
-    print '\n'
+    print '\nNumber of iterations actually performed:', (cycles_pc/cores) * cores
 
 
