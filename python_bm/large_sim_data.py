df479b224e28e1817addb4ab016802a0dc0bca39
dani <dani@a2f3893c-e248-4646-8fd3-e29ca8a5c454>
2011-01-19 04:21:20 +0000

    Changes in the script to benchmark with simulated large datasets

--- python_bm/large_sim_data.py
+++ python_bm/large_sim_data.py
@@ -11,14 +11,14 @@ from econometrics.testing_utils import Test_Data as Data
 
 print '\n\t\t\t### Large simulated dataset benchmarking ###\n'
 
-s = 1000 # Side of the lattice
+s = 1100 # Side of the lattice
 k = 10
 print 'n: %i'%s**2
 print 'k: %i'%k
 
 ti = time.time()
 t0 = time.time()
-data = Data(s**2, k, 'large', '../../../trunk/')
+data = Data(s**2, k, 'large', '../../../trunk/econometrics/', omit_w=True)
 t1 = time.time()
 tf = t1 - t0
 print 'Simulated data set created:\t%.5f seconds'%tf
@@ -29,8 +29,12 @@ t1 = time.time()
 tf = t1 - t0
 print 'Weights object created:\t\t%.5f seconds'%tf
 
-ols = 
+t0 = time.time()
+ols = OLS(data.y, data.x, w)
+t1 = time.time()
+tf = t1 - t0
+print 'Regression:\t\t\t%.5f seconds'%tf
 
 tff=time.time()
 tt = tff - ti
-print 'Total final time:\t%.5f seconds'%tt
+print 'Total final time:\t\t%.5f seconds'%tt
