482bad2df255f5517c9f9be76bd4e48de8e07857
dani <dani@a2f3893c-e248-4646-8fd3-e29ca8a5c454>
2011-01-24 17:01:08 +0000

    Large benchmarking in function form + adding script to plot results

--- python_bm/large_sim_data.py
+++ python_bm/large_sim_data.py
@@ -2,53 +2,174 @@
 Script to create large datasets and benchmark times
 """
 
-
 import pysal as ps
 import time
 import numpy as np
-from econometrics.ols import BaseOLS as OLS
-from econometrics.diagnostics_sp import LMtests, MoranRes
+from pysal.spreg.ols import OLS
+from pysal.spreg.ols import BaseOLS
+from pysal.spreg.diagnostics_sp import LMtests, MoranRes
 from econometrics.testing_utils import Test_Data as Data
 
 print '\n\t\t\t### Large simulated dataset benchmarking ###\n'
 
-s = 1500 # Side of the lattice
-k = 10
-print 'n: %i'%s**2
-print 'k: %i'%k
+def test_large_all(s, k, log):
+    """
+    Run and time OLS and various diagnostics
 
-ti = time.time()
-t0 = time.time()
-data = Data(s**2, k, 'large', '../../../trunk/econometrics/', omit_w=True)
-t1 = time.time()
-tf = t1 - t0
-print 'Simulated data set created:\t%.5f seconds'%tf
+    Arguments
+    ---------
+    s       : int
+              Side of the lattice from which to build the weights on
+              n = s**2
+    k       : int
+              N of variables to include in regression
+    log     : string
+              Path of log file to append results to
 
-t0 = time.time()
-w = ps.lat2W(s, s, rook=False)
-t1 = time.time()
-tf = t1 - t0
-print 'Weights object created:\t\t%.5f seconds'%tf
+    Returns
+    -------
+              Updates on-the-fly the log file with new timing results
+    """
+    log = open(log, 'a')
+    n = 'n: %i\n'%s**2
+    log.write(n)
+    print n
+    vars = 'k: %i\n'%k
+    log.write(vars)
+    print vars
 
-t0 = time.time()
-ols = OLS(data.y, data.x)
-t1 = time.time()
-tf = t1 - t0
-print 'Regression:\t\t\t%.5f seconds'%tf
+    ti = time.time()
+    t0 = time.time()
+    data = Data(s**2, k, 'large', '../../../trunk/econometrics/', omit_w=True)
+    t1 = time.time()
+    tf = t1 - t0
+    creDa = 'Create data:\t\t\t%.5f seconds\n'%tf
+    log.write(creDa)
+    print creDa
 
-t0 = time.time()
-lms = LMtests(ols, w)
-t1 = time.time()
-tf = t1 - t0
-print 'LM diagnostics:\t\t\t%.5f seconds'%tf
+    t0 = time.time()
+    w = ps.lat2W(s, s, rook=False)
+    t1 = time.time()
+    tf = t1 - t0
+    creWe = 'Created Weights:\t\t%.5f seconds\n'%tf
+    log.write(creWe)
+    print creWe
 
-t0 = time.time()
-lms = MoranRes(ols, w, z=True)
-t1 = time.time()
-tf = t1 - t0
-print 'Moran test:\t\t\t%.5f seconds'%tf
+    t0 = time.time()
+    ols = OLS(data.y, data.x)
+    t1 = time.time()
+    tf = t1 - t0
+    runOls = 'Regression:\t\t\t%.5f seconds\n'%tf
+    log.write(runOls)
+    print runOls
+
+    t0 = time.time()
+    lms = LMtests(ols, w)
+    t1 = time.time()
+    tf = t1 - t0
+    runLm = 'LM diagnostics:\t\t\t%.5f seconds\n'%tf
+    log.write(runLm)
+    print runLm
+
+    t0 = time.time()
+    lms = MoranRes(ols, w, z=True)
+    t1 = time.time()
+    tf = t1 - t0
+    runMoran = 'Moran test:\t\t\t%.5f seconds\n'%tf
+    log.write(runMoran)
+    print runMoran
 
 
-tff=time.time()
-tt = tff - ti
-print 'Total final time:\t\t%.5f seconds'%tt
+    tff=time.time()
+    tt = tff - ti
+    total = 'Total final time:\t\t%.5f seconds\n'%tt
+    log.write(total)
+    print total
+    log.close()
+    return 'Done!'
+
+#test_large_all(1100, 10, 'log.txt')
+
+def test_large_sp(s, k, log):
+    """
+    Run and time OLS and spatial diagnostics
+    ...
+
+    Arguments
+    ---------
+    s       : int
+              Side of the lattice from which to build the weights on
+              n = s**2
+    k       : int
+              N of variables to include in regression
+    log     : string
+              Path of log file to append results to
+
+    Returns
+    -------
+              Updates on-the-fly the log file with new timing results
+    """
+    log = open(log, 'a')
+    n = 'n: %i\n'%s**2
+    log.write(n)
+    print n
+    vars = 'k: %i\n'%k
+    log.write(vars)
+    print vars
+
+    ti = time.time()
+    t0 = time.time()
+    data = Data(s**2, k, 'large', '../../../trunk/econometrics/', omit_w=True)
+    t1 = time.time()
+    tf = t1 - t0
+    creDa = 'Create data:\t\t\t%.5f seconds\n'%tf
+    log.write(creDa)
+    print creDa
+
+    t0 = time.time()
+    w = ps.lat2W(s, s, rook=False)
+    t1 = time.time()
+    tf = t1 - t0
+    creWe = 'Created Weights:\t\t%.5f seconds\n'%tf
+    log.write(creWe)
+    print creWe
+
+    t0 = time.time()
+    ols = BaseOLS(data.y, data.x)
+    t1 = time.time()
+    tf = t1 - t0
+    runOls = 'Regression:\t\t\t%.5f seconds\n'%tf
+    log.write(runOls)
+    print runOls
+
+    t0 = time.time()
+    lms = LMtests(ols, w)
+    t1 = time.time()
+    tf = t1 - t0
+    runLm = 'LM diagnostics:\t\t\t%.5f seconds\n'%tf
+    log.write(runLm)
+    print runLm
+
+    t0 = time.time()
+    lms = MoranRes(ols, w, z=True)
+    t1 = time.time()
+    tf = t1 - t0
+    runMoran = 'Moran test:\t\t\t%.5f seconds\n'%tf
+    log.write(runMoran)
+    print runMoran
+
+
+    tff=time.time()
+    tt = tff - ti
+    total = 'Total final time:\t\t%.5f seconds\n'%tt
+    log.write(total)
+    print total
+    log.close()
+    return 'Done!'
+
+sizes = [150, 300, 450, 600, 750, 800, 900, 1000]
+sizes = [1150, 1300, 1450, 1600, 1750, 1900, 2000]
+for size in sizes:
+    test_large_sp(1450, 10, 'log_sp.txt')
+
+
