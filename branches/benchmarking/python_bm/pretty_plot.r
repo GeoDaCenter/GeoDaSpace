6bfb5a501504549b0d5729fdaac380e745fc61f6
dani <dani@a2f3893c-e248-4646-8fd3-e29ca8a5c454>
2011-01-28 22:28:49 +0000

    More playing with ggplot2

--- branches/benchmarking/python_bm/pretty_plot.r
+++ branches/benchmarking/python_bm/pretty_plot.r
@@ -7,15 +7,28 @@ r <- read.csv(r)
 spreg <- 'spregLog.csv'
 spreg <- read.csv(spreg)
 
-p <- ggplot(r, aes(x=n, y=creDa))
-p <- p + geom_area(aes(x=n, y=creDa))
-#   p <- p + geom_area(data=csv, aes(x=V3, fill=as.factor(1),alpha=0.5))
-#   p <- p + geom_area(data=csv, aes(x=V4, fill=as.factor(2),alpha=0.5))
+p <- ggplot(r)
+p <- p + geom_area(aes(x=n, y=reg, fill=as.factor(1), alpha=0.4))
+p <- p + geom_area(aes(x=n, y=lm, fill=as.factor(2), colour=as.factor(2), alpha=0.4))
+p <- p + geom_area(aes(x=n, y=moran, fill=as.factor(3), colour=as.factor(3), alpha=0.4))
 p <- p + scale_fill_discrete(
-   legend=T,
-   name="Distribución N. publicaciones",
-   breaks=as.factor(c(0,1,2)), 
-   labels=c('Pre-tesis','Tesis-6','>6 años post-tesis')
+    #palette=1,
+    legend=T,
+    name="Performance",
+    breaks=as.factor(c(1,2,3)), 
+    labels=c('Regression','LM Diagnostics', 'Residual Moran')
 )
+p <- p + scale_alpha(legend=F)
+p <- p + scale_colour(legend=T)
+
+p <- p + opts(panel.grid.major = theme_blank())
+#p <- p + opts(panel.grid.minor = theme_blank())
+#p <- p + opts(panel.background = theme_blank())
+p <- p + labs(x='N', y='Time')
+p <- p + opts(panel.border = theme_blank())
+
+
 
 ggsave(p,file="ggplot.png",width=7,height=5)
+
+print(p)
