2b11b90dc5ae4dbc55c1516762b66d9aba9e7de0
dani <dani@a2f3893c-e248-4646-8fd3-e29ca8a5c454>
2011-01-30 06:17:30 +0000

    Working on Seattle data. Fails to build the OLS summary printout because White test is 'N/A'

--- python_bm/seattle_bm.py
+++ python_bm/seattle_bm.py
@@ -7,50 +7,67 @@ NOTE: it tests functionality to be included in PySAL release 1.1 in Jan. 2011
 import pysal
 import time
 import numpy as np
-from econometrics.ols import OLS
+from pysal.spreg.ols import BaseOLS as OLS
+#from pysal.spreg.ols import OLS
 
-data_link = '/home/dani/AAA/LargeData/Seattle/parcel2000_city_resbldg99clean'
-data_link = '/Users/dani/AAA/LargeData/Seattle/parcel2000_city_resbldg99clean'
+def run_seattle(data_link, yvar, xvars, k=8, pts=True):
+    print '\n\t\t\t### Seattle dataset benchmarking ###\n'
 
-print '\n\t\t\t### Seattle dataset benchmarking ###\n'
+    ti = time.time()
+    t0 = time.time()
+    w_link = data_link
+    if pts:
+        w_link += '_pts'
+    w_link += '.shp'
+    w = pysal.knnW_from_shapefile(w_link, k=k)
+    t1 = time.time()
+    print 'Number of observations:\t\t%i\n'%w.n
+    tf = t1 - t0
+    print 'Shape reading and W creating:\t%.5f seconds'%tf
 
-ti = time.time()
-t0 = time.time()
-w = pysal.open(data_link + '_pts.gwt').read()
-#w = pysal.knnW_from_shapefile(data_link + '_pts.shp', k=4)
-t1 = time.time()
-print 'Number of observations:\t\t%i\n'%w.n
-tf = t1 - t0
-print 'Shape reading and W creating:\t%.5f seconds'%tf
+    t0 = time.time()
+    nat = pysal.open(data_link + '.dbf')
+    t1 = time.time()
+    tf = t1 - t0
+    print 'Loading data:\t\t\t%.5f seconds'%tf
 
-t0 = time.time()
-nat = pysal.open(data_link + '.dbf')
-t1 = time.time()
-tf = t1 - t0
-print 'Loading data:\t\t\t%.5f seconds'%tf
+    t0 = time.time()
+    y = np.array([nat.by_col(yvar)]).T
+    y = np.array(y, dtype=float)
+    t1 = time.time()
+    tf = t1 - t0
+    print 'Creating dep var y:\t\t%.5f seconds'%tf
 
-t0 = time.time()
-y = np.array([nat.by_col('PIN')]).T
-y = np.array(y, dtype=float)
-t1 = time.time()
-tf = t1 - t0
-print 'Creating dep var y:\t\t%.5f seconds'%tf
+    t0 = time.time()
+    x = map(nat.by_col, xvars)
+    x = map(np.array, x)
+    x = np.vstack(x)
+    x = np.array(x.T, dtype=float)
+    t1 = time.time()
+    tf = t1 - t0
+    print 'Creating indep vars x:\t\t%.5f seconds'%tf
 
-t0 = time.time()
-xvars = ['PARKEY', 'MAJOR', 'MINOR', 'LOT_SQFT', 'OID_', 'STORIES', 'DINEROOM', 'OTHERRM', 'BEDROOM']
-x = map(nat.by_col, xvars)
-x = map(np.array, x)
-x = np.vstack(x)
-x = x.T
-x = np.array(a, dtype=float)
-t1 = time.time()
-tf = t1 - t0
-print 'Creating indep vars x:\t\t%.5f seconds'%tf
+    t0 = time.time()
+    ols = OLS(y, x)
+    #ols = OLS(y, x, name_y=yvar, name_x=xvars, name_ds=data_link.split('/')[-1], vm=True)
+    t1 = time.time()
+    tf = t1 - t0
+    print 'Running OLS & diagnostics:\t%.5f seconds\n'%tf
+    #return [ols]
+    return [y, x]
 
-t0 = time.time()
-#ols = OLS(y, x, w, name_y='HR60', name_x=xvars, name_ds='NAT', vm=True)
-ols = OLS(y, x, name_y='PIN', name_x=xvars, name_ds='NAT', vm=True)
-t1 = time.time()
-tf = t1 - t0
-print 'Running OLS & diagnostics:\t%.5f seconds\n'%tf
+'''
+data_link = '/home/dani/AAA/LargeData/Seattle/parcel2000_city_resbldg99_larea'
+xvars = ['GRADE', 'BEDROOM', 'BASEAREA', 'FULLBATH', 'YEARBLT', 'CONDTION']
+yvar = 'LIVEAREA'
+model = run_seattle(data_link, yvar, xvars)
+
+'''
+data_link = '/home/dani/AAA/LargeData/Seattle/all97_Aug08_bis_resarea_tract'
+xvars = ['firplace', 'bricksto', 'bathfull', 'lake1k', 'park250', 'stories2', \
+    'viewall', 'nowhite', 'pcty65', 'pctcol', 'age', 'higrade', 'mindist_li', \
+    'sqfttotl_r', 'sqftlive_r', 'apex', 'wx_agep', 'wx_conpr', 'wx_sfhom', \
+    'wx_duplx']
+yvar = 'lnprice'
+model = run_seattle(data_link, yvar, xvars, pts=False)
 
