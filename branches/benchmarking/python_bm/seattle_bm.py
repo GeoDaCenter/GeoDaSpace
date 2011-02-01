0b420b1fcba136256f7289409dcac86ce84b62da
dani <dani@a2f3893c-e248-4646-8fd3-e29ca8a5c454>
2011-02-01 00:13:07 +0000

    Including knn=20 in Seatle test. Results:
    padma:python_bm dani$ python seattle_bm.py 
    
    ### Seattle dataset benchmarking ###
    
    Number of observations:137259
    
    Number of variables:20
    
    Shape reading and W creating:171.20384 seconds
    Loading data:0.00385 seconds
    (137259, 1) (137259, 6)
    Creating y & x vars:36.87874 seconds
    Regression:0.07846 seconds
    
    LM diagnostics:5.16200 seconds
    
    Moran test:0.70117 seconds
    
    Total final time:214.02970 seconds
    
    padma:python_bm dani$

--- branches/benchmarking/python_bm/seattle_bm.py
+++ branches/benchmarking/python_bm/seattle_bm.py
@@ -23,93 +23,74 @@ def run_seattle(data_link, yvar, xvars, k=8, pts=True):
     w = pysal.knnW_from_shapefile(w_link, k=k)
     t1 = time.time()
     print 'Number of observations:\t\t%i\n'%w.n
+    print 'Number of variables:\t\t%i\n'%k
     tf = t1 - t0
     print 'Shape reading and W creating:\t%.5f seconds'%tf
-
     t0 = time.time()
     nat = pysal.open(data_link + '.dbf')
     t1 = time.time()
     tf = t1 - t0
     print 'Loading data:\t\t\t%.5f seconds'%tf
-
     t0 = time.time()
-    y = np.array([nat.by_col(yvar)]).T
-    y = np.array(y, dtype=float)
+    yx = [yvar]
+    yx.extend(xvars)
+    yx = map(nat.header.index, yx)
+    yx = [map(row.__getitem__, yx) for row in nat]
+    yx = np.array(yx)
+    y, x = np.reshape(yx[:, 0], (w.n, 1)), yx[:, 1:]
+    print y.shape, x.shape
     t1 = time.time()
     tf = t1 - t0
-    print 'Creating dep var y:\t\t%.5f seconds'%tf
-
-    t0 = time.time()
-    tp0 = time.time()
-    x = map(nat.by_col, xvars)
-    tp1 = time.time()
-    tp = tp1 - tp0
-    print 'by_col: %f'%tp
-    x = np.array(x).T
-    #x = map(np.array, x)
-    #x = np.vstack(x)
-    #x = np.array(x.T, dtype=float)
-    t1 = time.time()
-    arr = t1 - tp1
-    print 'convert to array: %f'%arr
-    tf = t1 - t0
-    print 'Creating indep vars x:\t\t%.5f seconds'%tf
-   #t0 = time.time()
-   #x = map(nat.by_col, xvars)
-   #x = map(np.array, x)
-   #x = np.vstack(x)
-   #x = np.array(x.T, dtype=float)
-   #t1 = time.time()
-   #tf = t1 - t0
-   #print 'Creating indep vars x:\t\t%.5f seconds'%tf
-
+    print 'Creating y & x vars:\t\t%.5f seconds'%tf
     t0 = time.time()
     ols = OLS(y, x)
     t1 = time.time()
     tf = t1 - t0
     runOls = 'Regression:\t\t\t%.5f seconds\n'%tf
     print runOls
-
     t0 = time.time()
     lms = LMtests(ols, w)
     t1 = time.time()
     tf = t1 - t0
     runLm = 'LM diagnostics:\t\t\t%.5f seconds\n'%tf
     print runLm
-
     t0 = time.time()
     moran = MoranRes(ols, w, z=True)
     t1 = time.time()
     tf = t1 - t0
     runMoran = 'Moran test:\t\t\t%.5f seconds\n'%tf
     print runMoran
-
-   #t0 = time.time()
-   #ols = OLS(y, x)
-   #ols = OLS(y, x, name_y=yvar, name_x=xvars, name_ds=data_link.split('/')[-1], vm=True)
-   #t1 = time.time()
-   #tf = t1 - t0
-   #print 'Running OLS & diagnostics:\t%.5f seconds\n'%tf
-
     tff=time.time()
     tt = tff - ti
     total = 'Total final time:\t\t%.5f seconds\n'%tt
     print total
-
     return [ols]
 
-data_link = '/home/dani/AAA/LargeData/Seattle/parcel2000_city_resbldg99_larea'
-xvars = ['GRADE', 'BEDROOM', 'BASEAREA', 'FULLBATH', 'YEARBLT', 'CONDTION']
-yvar = 'LIVEAREA'
-model = run_seattle(data_link, yvar, xvars)
+larea = '/home/dani/AAA/LargeData/Seattle/parcel2000_city_resbldg99_larea'
+larea = '/Volumes/GeoDa/Workspace/Julia/Seattle/parcel2000_city_resbldg99_larea'
+
+resarea = '/home/dani/AAA/LargeData/Seattle/all97_Aug08_bis_resarea_tract'
+resarea = '/Volumes/GeoDa/Workspace/Julia/Seattle/all97_Aug08_bis_resarea_tract'
 
 '''
-data_link = '/home/dani/AAA/LargeData/Seattle/all97_Aug08_bis_resarea_tract'
+xvars = ['GRADE', 'BEDROOM', 'BASEAREA', 'FULLBATH', 'YEARBLT', 'CONDTION']
+yvar = 'LIVEAREA'
+# K= 8
+model = run_seattle(larea, yvar, xvars)
+
 xvars = ['firplace', 'bricksto', 'bathfull', 'lake1k', 'park250', 'stories2', \
     'viewall', 'nowhite', 'pcty65', 'pctcol', 'age', 'higrade', 'mindist_li', \
     'sqfttotl_r', 'sqftlive_r', 'apex', 'wx_agep', 'wx_conpr', 'wx_sfhom', \
     'wx_duplx']
 yvar = 'lnprice'
-model = run_seattle(data_link, yvar, xvars, pts=False)
-'''
+model = run_seattle(resarea, yvar, xvars, pts=False)
 
+'''
+# K= 20
+data_link = '/home/dani/AAA/LargeData/Seattle/parcel2000_city_resbldg99_larea'
+data_link = '/Volumes/GeoDa/Workspace/Julia/Seattle/parcel2000_city_resbldg99_larea'
+xvars = ['GRADE', 'BEDROOM', 'BASEAREA', 'FULLBATH', 'YEARBLT', 'CONDTION']
+yvar = 'LIVEAREA'
+model = run_seattle(data_link, yvar, xvars, k=20)
+
+# Min. Threshold
