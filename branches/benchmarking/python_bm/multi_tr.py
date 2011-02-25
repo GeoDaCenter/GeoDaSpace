ed28ba58b3ed43bd182cb54c1d78c5755cbca9a5
dani <dani@a2f3893c-e248-4646-8fd3-e29ca8a5c454>
2011-02-25 19:10:12 +0000

    minor changes to the processing testing

--- branches/benchmarking/python_bm/multi_tr.py
+++ branches/benchmarking/python_bm/multi_tr.py
@@ -3,20 +3,35 @@ Playing with multiprocessing to offload trace computation
 '''
 
 from multiprocessing import Process
+import multiprocessing as mp
 import numpy as np
 import pysal as ps
+import time
 
 def trWpW(w):
-    tr = w.sparse.T * w.sparse
+    tr = w.T * w
     return np.sum(tr.diagonal())
 
 if __name__ == '__main__':
 
-    w = ps.lat2W(1750, 1750)
-    print 'N: %i'%w.n
+    s = 3000
+    x = np.random.random((s**2, 100))
+    xx = np.random.random((s**2, 100))
+    y = np.random.random((s**2, 1))
+    w = ps.weights.lat2SW(s, s)
+    print 'N: %i'%s**2
     tr0 = trWpW(w)
     print 'Trace computed in current thread'
-    p = Process(target=trWpW, args=(w,))
+    '''
+    w2 = ps.weights.lat2SW(2000, 2000, criterion='rook')
+    print 'Created new w'
+    tr1 = trWpW(w2)
+    print 'Trace computed in current thread'
+    p = Process(target=trWpW, args=(w2,))
     p.start()
     p.join()
+    cores = mp.cpu_count()
+    pool = mp.Pool(cores)
+    trs = pool.map(trWpW, [w2])
+    '''
     print 'Trace computed in offloaded process'
