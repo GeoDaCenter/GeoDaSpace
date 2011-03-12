3b0d20ccb84464d4d817d55dd7657c2748cc3eeb
pedro <pedro@a2f3893c-e248-4646-8fd3-e29ca8a5c454>
2011-03-12 02:12:41 +0000

    Helper functions. All checked against original code.

--- madsen_pedro.py
+++ madsen_pedro.py
@@ -1,3 +1,54 @@
 '''
 Pedro's part of porting Madsen's code
 '''
+import numpy as np
+from scipy.special import beta, psi
+def p(y,phi,mu):
+    p=np.zeros((y.shape),float)
+    if mu.shape[0]==1:
+        mu=mu*np.ones((y.shape))
+    for i in range(y.shape[0]):
+        if y[i]==0:
+            p[i]=(phi**2./(1+phi**2))**(phi**2.*mu[i])
+        else:
+            p[i]=1./(y[i]*beta(y[i],phi**2.*mu[i]))*(phi**2/(1+phi**2))**(phi**2.*mu[i])*(1./(1+phi**2)**y[i]) #beta?
+    return p
+
+def dpdphi(y,phi,mu):
+    dpdphi=np.zeros((y.shape),float)
+    if mu.shape[0]==1:
+        mu=mu*np.ones((y.shape))
+    for i in range(y.shape[0]):
+        phi2 = phi**2.
+        prodphi = phi2/(1+phi2)
+        if y[i]==0:
+            fac1 = prodphi**(phi2*mu[i])
+            fac2 = (2.*phi*mu[i]*np.log(prodphi)+mu[i]*(2.*phi/(1+phi2)-2.*phi**3./(1+phi2)**2)*(1+phi2))
+            fac3 = ((1+phi2)**y[i])-2.*prodphi**(phi2*mu[i])/((1+phi2)**y[i])*y[i]*phi/(1+phi2)
+            dpdphi[i] = fac1*fac2/fac3
+        else:
+            fac1 = -1./y[i]/beta(phi2*mu[i],y[i])*prodphi**(phi2*mu[i])/((1+phi2)**y[i])
+            fac2 = 2.*phi*mu[i]*psi(phi2*mu[i])-2.*phi*mu[i]*psi(y[i]+phi2*mu[i])
+            fac3 = 1./y[i]/beta(phi2*mu[i],y[i])*prodphi**(phi2*mu[i])
+            fac4 = 2.*phi*mu[i]*np.log(prodphi)+mu[i]*(2.*phi/(1+phi2)-2.*phi**3./(1+phi2)**2)*(1+phi**2)
+            fac5 = ((1+phi2)**y[i])-2./beta(phi2*mu[i],y[i])
+            fac6 = prodphi**(phi2*mu[i])/((1+phi2)**y[i])*phi/(1+phi2)
+            dpdphi[i] = fac1*fac2+fac3*fac4/fac5*fac6
+    return dpdphi
+
+def dpdmu=dpdmu(y,phi,mu):           
+    dpdmu=np.zeros((y.shape),float)    
+    if mu.shape[0]==1:
+        mu=mu*np.ones((y.shape))
+    for i in range(y.shape[0]):
+        phi2 = phi**2.
+        prodphi = phi2/(1+phi2)
+        if y[i]==0:
+            dpdmu[i]=prodphi**(phi2*mu[i])*phi2*np.log(prodphi)
+        else:
+            fac1 = -1./y[i]/beta(y[i],phi2*mu[i])*prodphi**(phi2*mu[i])/((1+phi2)**y[i])
+            fac2 = phi2*psi(phi2*mu[i])-phi2*psi(y[i]+phi2*mu[i])
+            fac3 = 1./y[i]/beta(y[i],phi2*mu[i])*prodphi**(phi2*mu[i])
+            fac4 = phi2*np.log(prodphi)/((1+phi2)**y[i])
+            dpdmu[i] = fac1*fac2+fac3*fac4
+    return dpdmu
