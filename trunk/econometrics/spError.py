ddefe0edc65e2a697e6712b5b619467d50e2fe76
pedro <pedro@a2f3893c-e248-4646-8fd3-e29ca8a5c454>
2011-03-22 18:26:51 +0000

    Cleaning unrequired functions.

--- trunk/econometrics/spError.py
+++ trunk/econometrics/spError.py
@@ -111,7 +111,7 @@ class GMSWLS:
 
         #1b. GMM --> \tilde{\lambda1}
         moments = _momentsGMSWLS(w, ols.u)
-        lambda1 = self._optimizer_gmswls(moments)[0][0]
+        lambda1 = optim_moments(moments)
 
         #2a. OLS -->\hat{betas}
         xs,ys = get_spFilter(w, lambda1, x),get_spFilter(w, lambda1, y)
@@ -126,47 +126,6 @@ class GMSWLS:
 
         self.se_betas, self.z, self.pvals = _inference(ols)
         self.step2OLS = ols
-
-    def _optimizer_gmswls(self, moments):
-        """
-        Optimization of moments
-        ...
-
-        Parameters
-        ----------
-
-        moments     : _momentsGMSWLS
-                      Instance of _momentsGMSWLS with G and g
-        vcX         : array
-                      Optional. 2x2 array with the Variance-Covariance matrix to be used as
-                      weights in the optimization (applies Cholesky
-                      decomposition). Set empty by default.
-
-        Returns
-        -------
-        x, f, d     : tuple
-                      x -- position of the minimum
-                      f -- value of func at the minimum
-                      d -- dictionary of information from routine
-                            d['warnflag'] is
-                                0 if converged
-                                1 if too many function evaluations
-                                2 if stopped for another reason, given in d['task']
-                            d['grad'] is the gradient at the minimum (should be 0 ish)
-                            d['funcalls'] is the number of function calls made
-        """
-           
-        lambdaX = op.fmin_l_bfgs_b(self._gm_gmswls,[0.0, 0.0],args=[moments],approx_grad=True,bounds=[(-1.0,1.0), (0, None)])
-        return lambdaX
-
-    def _gm_gmswls(self, lambdapar, moments):
-        """
-        Preparation of moments for minimization in a GMSWLS framework
-        """
-        par=np.array([[float(lambdapar[0]),float(lambdapar[0])**2., lambdapar[1]]]).T
-        vv = np.dot(moments[0], par)
-        vv = vv - moments[1]
-        return sum(vv**2)
 
 class GSTSLS:
     '''
@@ -298,127 +257,6 @@ def _momentsGMSWLS(w, u):
     G = np.array([[2 * uwu[0][0], -wu2[0][0], w.n], [2 * wuwwu[0][0], -wwu2[0][0], trWtW], [uwwu[0][0] + wu2[0][0], -wuwwu[0][0], 0.]]) / w.n
 
     return [G, g]
-
-def get_A1(S):
-    """
-    Builds A1 as in Arraiz et al [1]_
-
-    .. math::
-
-        A_1 = W' W - diag(w'_{.i} w_{.i})
-
-    ...
-
-    Parameters
-    ----------
-
-    S               : csr_matrix
-                      PySAL W object converted into Scipy sparse matrix
-
-    Returns
-    -------
-
-    Implicit        : csr_matrix
-                      A1 matrix in scipy sparse format
-    References
-    ----------
-
-    .. [1] Arraiz, I., Drukker, D. M., Kelejian, H., Prucha, I. R. (2010) "A
-    Spatial Cliff-Ord-Type Model with Heteroskedastic Innovations: Small and
-    Large Sample Results". Journal of Regional Science, Vol. 60, No. 2, pp.
-    592-614.
-
-           
-    """
-    StS = S.T * S
-    d = SP.spdiags([StS.diagonal()], [0], S.get_shape()[0], S.get_shape()[1])
-    d = d.asformat('csr')
-    return StS - d
-
-def get_spFilter(w,lamb,sf):
-    '''
-    computer the spatially filtered variables
-    
-    Parameters
-    ----------
-    w       : weight
-              PySAL weights instance  
-    lamb    : double
-              spatial autoregressive parameter
-    sf      : array
-              the variable needed to compute the filter
-    Returns
-    --------
-    rs      : array
-              spatially filtered variable
-    
-    Examples
-    --------
-
-    >>> import numpy as np
-    >>> import pysal
-    >>> db=pysal.open("examples/columbus.dbf","r")
-    >>> y = np.array(db.by_col("CRIME"))
-    >>> y = np.reshape(y, (49,1))
-    >>> w=pysal.open("examples/columbus.gal").read()        
-    >>> solu = get_spFilter(w,0.5,y)
-    >>> print solu
-    [[  -8.9882875]
-     [ -20.5685065]
-     [ -28.196721 ]
-     [ -36.9051915]
-     [-111.1298   ]
-     [ -14.5570555]
-     [ -99.278625 ]
-     [ -86.0715345]
-     [-117.275209 ]
-     [ -16.655933 ]
-     [ -62.3681695]
-     [ -73.0446045]
-     [ -29.471835 ]
-     [ -71.3954825]
-     [-101.7297645]
-     [-154.623178 ]
-     [   9.732206 ]
-     [ -58.3998535]
-     [ -15.1412795]
-     [-162.0080105]
-     [ -25.5078975]
-     [ -74.007205 ]
-     [  -8.0705775]
-     [-153.8715795]
-     [-138.5858265]
-     [-104.918187 ]
-     [ -13.6139665]
-     [-156.4892505]
-     [-120.5168695]
-     [ -52.541277 ]
-     [ -11.0130095]
-     [  -8.563781 ]
-     [ -32.5883695]
-     [ -20.300339 ]
-     [ -76.6698755]
-     [ -32.581708 ]
-     [-110.5375805]
-     [ -77.2471795]
-     [  -5.1557885]
-     [ -36.3949255]
-     [ -12.69973  ]
-     [  -2.647902 ]
-     [ -71.81993  ]
-     [ -63.405917 ]
-     [ -35.1192345]
-     [  -0.1726765]
-     [  10.2496385]
-     [ -30.452661 ]
-     [ -18.2765175]]
-
-    '''        
-    # convert w into sparse matrix      
-    w_matrix = w.sparse
-    rs = sf - lamb * (w_matrix * sf)    
-    
-    return rs
 
 def _test():
     import doctest
