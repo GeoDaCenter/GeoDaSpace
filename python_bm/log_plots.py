bda491cc66783b3f631465f4b973ffa4e42006dd
dani <dani@a2f3893c-e248-4646-8fd3-e29ca8a5c454>
2011-04-01 22:04:12 +0000

    Code to generate performance graphs

--- python_bm/log_plots.py
+++ python_bm/log_plots.py
@@ -4,6 +4,72 @@ Read the logs from benchmarking and plot them
 
 import pylab as pl
 
+def load_log_py(log_file):
+    log = open(log_file)
+    n = []
+    k = []
+    creDa = []
+    creWe = []
+    gmswls = []
+    stsls = []
+    total = []
+    model = None
+    for line in log:
+        line = line.strip('\n').split()
+        if len(line) > 0:
+            if line[0] == 'Model:':
+                model = line[1]
+            if line[0] == 'n:':
+                n.append(line[1])
+            if line[0] == 'k:':
+                k.append(line[1])
+            if line[0] == 'Create':
+                creDa.append(line[2])
+            if line[0] == 'Created':
+                creWe.append(line[2])
+            if line[0] == 'GMSWLS:':
+                gmswls.append(float(line[1]))
+            if line[0] == 'STSLS:':
+                stsls.append(float(line[1]))
+            if line[0] == 'Total':
+                total.append(line[3])
+    log.close()
+    return model, n, k, creDa, creWe, gmswls, stsls, total
+
+def load_log_r(log_file):
+    log = open(log_file)
+    lines = log.readlines()
+    log.close()
+    n = []
+    k = []
+    creDa = []
+    creWe = []
+    total = []
+    gmswls = []
+    stsls = []
+    lm = []
+    model = None
+    for i in range(len(lines)):
+        line = lines[i].strip('\n').strip('[1] ').strip('"').split(' ')
+        if line[0] == 'Model:':
+            model = line[1]
+        if line[0] == 'N:':
+            n.append(float(line[1]))
+        if line[0] == 'k:':
+            k.append(line[1])
+        if line[0] == 'Create':
+            creDa.append(float(lines[i+2]))
+        if line[0] == 'Created':
+            creWe.append(float(lines[i+2]))
+        if line[0] == 'GMSWLS:':
+            gmswls.append(float(lines[i+2]))
+        if line[0] == 'STSLS:':
+            stsls.append(float(lines[i+2]))
+        if line[0] == 'Total':
+            total.append(float(lines[i+2]))
+    return model, n, k, creDa, creWe, gmswls, stsls, total
+
+
 def plot_all(log_file, title='SPREG benchmarking', pic=None):
     log = open(log_file)
     n = []
@@ -190,16 +256,18 @@ def write_r(log_file, outfile):
     fo.close()
     return pack
 
-#obj = write_all('log_sp.txt', outfile='spregLog.csv')
-#obj = write_r('large-data-simR.txt', outfile='rLog.csv')
+if __name__ == '__main__':
 
-#obj = plot_r('large-data-simR.txt', pic='log_sp_r.png')
+    #obj = write_all('log_sp.txt', outfile='spregLog.csv')
+    #obj = write_r('large-data-simR.txt', outfile='rLog.csv')
 
-#plot_all('log_sp.txt', title='SPREG benchmarking\n(only sp Diagnostics)', \
-#        pic='log_sp.png')
+    #obj = plot_r('large-data-simR.txt', pic='log_sp_r.png')
 
-#plot_all('log_all.txt', title='SPREG benchmarking\n(non-sp & sp Diagnostics)', \
-#       pic='log_all.png')
+    #plot_all('log_sp.txt', title='SPREG benchmarking\n(only sp Diagnostics)', \
+    #        pic='log_sp.png')
 
-#plot_all('large_sw.txt', title='SPREG lat2SW')
-write_all('large_sw.txt', 'large_sw_rplot.csv')
+    #plot_all('log_all.txt', title='SPREG benchmarking\n(non-sp & sp Diagnostics)', \
+    #       pic='log_all.png')
+
+    #plot_all('large_sw.txt', title='SPREG lat2SW')
+    write_all('large_sw.txt', 'large_sw_rplot.csv')
--- r_bm/large_sim_data.r
+++ r_bm/large_sim_data.r
@@ -3,7 +3,7 @@
 
 library(spdep)
 
-test.large.sp <- function(s, k){
+test.large.olsSPd <- function(s, k){
     n <- s**2
     mes <- paste('N:', n)
     print(mes)
@@ -52,16 +52,144 @@ test.large.sp <- function(s, k){
 
 }
 
+test.large.GMSWLS <- function(s, k){
+    n <- s**2
+    mes <- paste('N:', n)
+    print(mes)
+    model <- 'Model: GMSWLS'
+    print(model)
+
+    ti <- proc.time()
+    t0 <- proc.time()
+    y <- rnorm(n)
+    x <- replicate(k, rnorm(n))
+    t1 <- proc.time()
+    time <- t1 - t0
+    print('Create data:')
+    print(time[3])
+
+    t0 <- proc.time()
+    w <- cell2nb(s, s)
+    w <- nb2listw(w)
+    t1 <- proc.time()
+    time <- t1 - t0
+    print('Created weights:')
+    print(time[3])
+
+    t0 <- proc.time()
+    ols <- GMerrorsar(y ~ x, data=list(), w)
+    t1 <- proc.time()
+    time <- t1 - t0
+    print('Regression:')
+    print(time[3])
+
+    total <- t1 - ti
+    print('Total time:')
+    print(total[3])
+
+}
+
+test.large.STSLS  <- function(s, k){
+    n <- s**2
+    mes <- paste('N:', n)
+    print(mes)
+    model <- 'Model: STSLS'
+    print(model)
+
+    ti <- proc.time()
+    t0 <- proc.time()
+    y <- rnorm(n)
+    x <- replicate(k, rnorm(n))
+    t1 <- proc.time()
+    time <- t1 - t0
+    print('Create data:')
+    print(time[3])
+
+    t0 <- proc.time()
+    w <- cell2nb(s, s)
+    w <- nb2listw(w)
+    t1 <- proc.time()
+    time <- t1 - t0
+    print('Created weights:')
+    print(time[3])
+
+    t0 <- proc.time()
+    ols <- stsls(y ~ x, data=list(), w)
+    t1 <- proc.time()
+    time <- t1 - t0
+    print('Regression:')
+    print(time[3])
+
+    total <- t1 - ti
+    print('Total time:')
+    print(total[3])
+
+}
+
+test.large.sp.models  <- function(s, k){
+    n <- s**2
+    mes <- paste('N:', n)
+    print(mes)
+    model <- 'Model: STSLS'
+    print(model)
+
+    ti <- proc.time()
+    t0 <- proc.time()
+    y <- rnorm(n)
+    x <- replicate(k, rnorm(n))
+    t1 <- proc.time()
+    time <- t1 - t0
+    print('Create data:')
+    print(time[3])
+
+    t0 <- proc.time()
+    w <- cell2nb(s, s)
+    w <- nb2listw(w)
+    t1 <- proc.time()
+    time <- t1 - t0
+    print('Created weights:')
+    print(time[3])
+
+    t0 <- proc.time()
+    ols <- GMerrorsar(y ~ x, data=list(), w)
+    t1 <- proc.time()
+    time <- t1 - t0
+    print('GMSWLS:')
+    print(time[3])
+
+    t0 <- proc.time()
+    ols <- stsls(y ~ x, data=list(), w)
+    t1 <- proc.time()
+    time <- t1 - t0
+    print('STSLS:')
+    print(time[3])
+
+    total <- t1 - ti
+    print('Total time:')
+    print(total[3])
+
+}
+
 k <- 10
 sizes <- cbind(150, 300, 450, 600, 750, 800, 900, 1000)
 sizes <- cbind(1150, 1300, 1450, 1600, 1750, 1900, 2000)
-#sizes <- cbind(150)
+sizes <- cbind(150, 300, 450)
+#sizes <- cbind(15)
 
 for(size in sizes){
     mes <- paste('Evaluating size:', size**2)
     print(mes)
-    sink('large-data-sim.txt', append=TRUE)
-    test.large.sp(size, k)
+#   sink('logs/gmswls_r.log', append=TRUE)
+#   test.large.GMSWLS(size, k)
+#   sink()
+
+#   sink('logs/stsls_r.log', append=TRUE)
+#   test.large.STSLS(size, k)
+#   sink()
+
+    sink('logs/sp_models.log', append=TRUE)
+    test.large.sp.models(size, k)
     sink()
+
 }
 
