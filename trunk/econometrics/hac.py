78b77caa56dddc082af3d16307265e9bc8fa55fa
luc <luc@a2f3893c-e248-4646-8fd3-e29ca8a5c454>
2011-04-07 05:14:56 +0000

    hac.py is annotated with correct matrix notation -- needs to be adjusted to spreg objects

--- trunk/econometrics/hac.py
+++ trunk/econometrics/hac.py
@@ -129,20 +129,21 @@
            hacflag: 1 for HAC, 0 for White       
            returns HAC variance
         """        # initialize X as array
-        kx = nm.array(self.X)
+        kx = nm.array(self.X)            # N by K array of X
         # initialize residuals as array
-        ke = nm.array(self.results['e'])
+        ke = nm.array(self.results['e'])   # N by 1 array of OLS residuals
         # kernel cross-products
-        kxe = kx * ke
+        kxe = kx * ke   # this is not matrix multiplication but each X is multiplied by e, so x(ik)*e(i)
         if hacflag:
-            wkxe = self.kw1.ksplagl(kxe.tolist())
-            wkxe = nm.array(wkxe)
-            kphi = nm.inner(kxe,wkxe)
+            wkxe = self.kw1.ksplagl(kxe.tolist())     # this applies KW to kx*ke so to the product of xik.ei
+            wkxe = nm.array(wkxe)  # side effect of moving between lists and arrays
+            kphi = nm.inner(kxe,wkxe)  # the cross product of the Xe times the Wxe or Sumi Sumj xi xj k(ij) ei ej
+            # eq 8 on p 141 of KP
         else:
-            kphi = nm.inner(kxe,kxe)
+            kphi = nm.inner(kxe,kxe)  # just the cross product of x*e for White
         # variance matrix as (X'X)-1 Phi (X'X)-1
-        kdd = nm.dot(kphi,self.results['xd'])
-        phi = nm.dot(self.results['xd'],kdd)
+        kdd = nm.dot(kphi,self.results['xd'])   # xd is (X'X)-1 so this is Phi*(X'X)-1 
+        phi = nm.dot(self.results['xd'],kdd) # this is (X'X)-1 (XeKXe) (X'X)-1 the variance matrix for OLS case
         return phi
         
         
@@ -153,26 +154,28 @@
            returns HAC variance
         """
         # initialize X as array
-        kx = nm.array(self.H)
+        kx = nm.array(self.H) # N by P array of instruments H
         # initialize residuals as array
-        ke = nm.array(self.results['e'])
+        ke = nm.array(self.results['e']) # N by 1 array of 2SLS residuals
         # kernel cross-products
-        kxe = kx * ke
+        kxe = kx * ke   # note kx here is H so this is h(ik)*e(i)
         #print 'THIS IS KXE'
         #print kxe
         if hacflag:
-            wkxe = self.kw1.ksplagl(kxe.tolist())
+            wkxe = self.kw1.ksplagl(kxe.tolist())  # kernel lag applied to the He
             #print 'this is wkxe'
             #print wkxe
-            wkxe = nm.array(wkxe)
-            kphi = nm.inner(kxe,wkxe)
+            wkxe = nm.array(wkxe)  # need to turn into an array from a list -- not crucial
+            kphi = nm.inner(kxe,wkxe)  # cross product Sumi Sum hir hjs ui uj K(ij)  equation 8 p. 141 in KP
         else:
-            kphi = nm.inner(kxe,kxe)
+            kphi = nm.inner(kxe,kxe)  # just the white thing -- we may not need
         # variance matrix as (Zh'Zh)-1Z'H(H'H)-1 Phi (H'H)-1 H'Z(Zh'Zh)-1
-        kdd = nm.dot(self.results['xdp'],kphi)
+        # see theorem 3
+        kdd = nm.dot(self.results['xdp'],kphi)  # xdp is  [Z'H (H'H)-1 H'Z]-1 Z'H(H'H)-1 kphi is Phi
         print 'THIS IS KDD'
         print kdd
-        phi = nm.dot(kdd,nm.transpose(self.results['xdp']))
+        phi = nm.dot(kdd,nm.transpose(self.results['xdp']))  #  [Z'H (H'H)-1 H'Z]-1 Z'H(H'H)-1 kphi (H'H)-1 H'Z [Z'H (H'H)-1 H'Z]-1 
+        # see proof of theorem 3 p. 151 Zhat'Zhat is replaced by explicit expression
         return phi
         
         
