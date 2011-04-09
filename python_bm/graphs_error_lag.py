355f35bfb9f5051a2d474af440e69afa5a4861a0
dani <dani@a2f3893c-e248-4646-8fd3-e29ca8a5c454>
2011-04-09 02:42:40 +0000

    Adding mixed graphs and fine tuning

---
+++ python_bm/graphs_error_lag.py
@@ -0,0 +1,73 @@
+'''
+Plot performance in computing STSLS and GMSWLS
+'''
+
+from log_plots import load_log_py, load_log_r
+import matplotlib.pylab as plt
+import numpy as np
+import os
+
+if os.uname()[0] == 'Darwin':
+    print 'Running MacOSX'
+    comp = '/Users/'
+elif os.uname()[0] == 'Linux':
+    print 'Running Linux'
+    comp = '/home/'
+
+def convert(x, lims):
+    return x / (lims[1] - lims[0])
+
+py_link = comp + 'dani/Dropbox/aagLogs/gmswls_py.log'
+model_py, n_py, k_py, creDa_py, creWe_py, ols_py, lm_py, moran_py, gmswls_py, stsls_py, total_py = load_log_py(py_link)
+r_link = comp + 'dani/Dropbox/aagLogs/gmswls_r.log'
+model_r, n_r, k_r, creDa_r, creWe_r, ols_r, lm_r, moran_r, gmswls_r, stsls_r, total_r = load_log_r(r_link)
+
+reg_fig = plt.figure(1)
+reg_sub = plt.subplot(111)
+
+#plt.plot(-1, -1, color='white')
+
+plt.plot(n_r[:len(gmswls_r)], gmswls_r, label='R', color='red', lw=2)
+plt.text(n_r[len(gmswls_r) - 1], gmswls_r[-1], 'GM Error')
+plt.axvline(x = n_r[len(gmswls_r) - 1], ymax = 
+        convert(gmswls_r[-1]-reg_sub.get_ylim()[0], reg_sub.get_ylim()), 
+        c='black', lw=0.5, ls='--')
+
+plt.plot(n_py[:-1], gmswls_py, label='Spreg', color='blue', lw=2)
+plt.text(n_py[len(gmswls_py) - 1], gmswls_py[-1], 'GM Error',\
+        verticalalignment='center')
+plt.axvline(x = n_py[len(gmswls_py) - 1], ymax = 
+        convert(gmswls_py[-1]-reg_sub.get_ylim()[0], reg_sub.get_ylim()), 
+        c='black', lw=0.5, ls='--')
+
+plt.legend(loc=2, frameon=False)
+
+py_link = comp + 'dani/Dropbox/aagLogs/stsls_py.log'
+model_py, n_py, k_py, creDa_py, creWe_py, ols_py, lm_py, moran_py, gmswls_py, stsls_py, total_py = load_log_py(py_link)
+r_link = comp + 'dani/Dropbox/aagLogs/stsls_r.log'
+model_r, n_r, k_r, creDa_r, creWe_r, ols_r, lm_r, moran_r, gmswls_r, stsls_r, total_r = load_log_r(r_link)
+
+plt.plot(n_r[:len(stsls_r)], stsls_r, color='red', lw=2)
+plt.text(n_py[len(stsls_r)], stsls_r[-1], 'GM Lag', verticalalignment='bottom')
+plt.axvline(x = n_r[len(stsls_r) - 1], ymax = 
+        convert(stsls_r[-1]-reg_sub.get_ylim()[0], reg_sub.get_ylim()), 
+        c='black', lw=0.5, ls='--')
+
+plt.plot(n_py[:len(stsls_py)], stsls_py, color='blue', lw=2)
+plt.text(n_py[len(stsls_py) - 1], stsls_py[-1], 'GM Lag', verticalalignment='center')
+
+xmin, xmax = plt.xlim()
+plt.xlim(-(xmax-xmin)*0.15, xmax)
+ymin, ymax = plt.ylim()
+plt.ylim(-(ymax-ymin)*0.15, ymax)
+
+xt = reg_sub.get_xticks()
+reg_sub.set_xticks(xt[1:])
+plt.suptitle("Computation time: Spatial Models", weight='bold')
+plt.xlabel('N')
+plt.ylabel('Seconds')
+plt.savefig(comp + 'dani/Dropbox/aagGraphs/sp_error_lag.png')
+
+#plt.show()
+
+
--- python_bm/graphs_weights.py
+++ python_bm/graphs_weights.py
@@ -4,6 +4,7 @@ Plot performance in creating lattice (queen) weights
 
 from log_plots import load_log_py, load_log_r
 import matplotlib.pylab as plt
+import numpy as np
 import os
 
 if os.uname()[0] == 'Darwin':
@@ -21,12 +22,24 @@ model_r, n_r, k_r, creDa_r, creWe_r, ols_r, lm_r, moran_r, gmswls_r, stsls_r, to
 
 reg_fig = plt.figure(1)
 reg_sub = plt.subplot(111)
-plt.plot(n_r[:len(creWe_r)], creWe_r, label='R')
-plt.plot(n_py[:len(creWe_py)], creWe_py, label='Spreg')
-plt.legend(loc=2)
-plt.title('Computation time to create lattice (queen) weights', weight='bold')
+plt.plot(n_r[:len(creWe_r)], creWe_r, label='R', lw=2, c='red')
+plt.plot(n_py[:len(creWe_py)], creWe_py, label='Spreg', lw=2, c='blue')
+
+xmin, xmax = plt.xlim()
+plt.xlim(-(xmax-xmin)*0.15, xmax)
+ymin, ymax = plt.ylim()
+plt.ylim(-(ymax-ymin)*0.15, ymax)
+
+locs, labels = plt.yticks()
+labs = np.arange(6)*1000 / 60
+lab = ['']
+lab.extend(map(str, map(round, labs)))
+plt.yticks(locs, tuple(lab))
+
+plt.legend(loc=2, frameon=False)
+plt.suptitle('Computation time to create lattice (queen) weights', weight='bold')
 plt.xlabel('N')
-plt.ylabel('Seconds')
+plt.ylabel('Minutes')
 plt.savefig(comp + 'dani/Dropbox/aagGraphs/weights.png')
 
 #plt.show()
