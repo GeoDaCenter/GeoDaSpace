2fde2ffaffd4ed197d3a50ae4fdbb1197e16556e
dani <dani@a2f3893c-e248-4646-8fd3-e29ca8a5c454>
2011-04-14 02:21:48 +0000

    Adding script to experiment with "copula" package in R

---
+++ copula_playing.r
@@ -0,0 +1,58 @@
+# Experiments w/ copula package in R
+
+library(copula)
+library(spdep)
+
+# Generate spatially autocorrelated data for a lattice of 10, 10
+lambda <- 0.5
+w <- cell2nb(10, 10)
+e <- runif(100)
+u <- invIrM(w, rho=lambda, method="chol", feasible=TRUE) %*% e
+wu <- lag.listw(nb2listw(w), u)
+
+# Graphical devices
+#mp <- moran.plot(u)
+#plot(u, wu)
+
+### Copula ###
+x <- matrix(cbind(u, wu), nrow=length(u), ncol=2)
+f <- pnorm(x)
+
+# Normal
+cop <- normalCopula(0.3,dim=2,dispstr="un")
+nor <- mvdc(copula=cop, 
+            margins=c('norm', 'norm'),
+            paramMargins=list(list(mean=0, sd=1), list(mean=0, sd=3))
+            )
+rn <- rmvdc(mvd, 1000)
+
+# Clayton
+clc <- archmCopula(family='clayton', dim=2, param=1.5)
+cl <- mvdc(copula=clc,
+            margins=c('norm', 'norm'),
+            paramMargins=list(list(mean=0, sd=1), list(mean=0, sd=1))
+            )
+rc <- rmvdc(cl, 1000)
+#plot(rn)
+#plot(rc)
+
+#c <- contour(cl, dmvdc, xlim=c(-3, 3), ylim=c(-3, 3))
+#points(rc)
+#c <- contour(nor, dmvdc, xlim=c(-3, 3), ylim=c(-3, 3))
+
+fitC <- fitMvdc(rc, cl, start=c(0, 1, 0, 1, 0))
+m1 <- fitC@estimate[1]
+v1 <- fitC@estimate[2]
+m2 <- fitC@estimate[3]
+v2 <- fitC@estimate[4]
+pa <- fitC@estimate[5]
+
+eclc <- archmCopula(family='clayton', dim=2, param=pa)
+resC <- mvdc(copula=eclc,
+            margins=c('norm', 'norm'),
+            paramMargins=list(list(mean=m1, sd=v1), list(mean=m2, sd=v2))
+            )
+c <- contour(resC, dmvdc, xlim=c(-3, 3), ylim=c(-3, 3))
+points(rc)
+
+
