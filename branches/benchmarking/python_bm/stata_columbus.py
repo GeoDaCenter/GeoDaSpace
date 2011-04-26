cbb7220be13654ca6015d1e9a63e60f3d8c8b623
dani <dani@a2f3893c-e248-4646-8fd3-e29ca8a5c454>
2011-04-26 19:16:19 +0000

    Adding R script to test results from STATA. S2SLS matches across all packages when w_lags=2. GM_Error matches in R and spreg but NOT in STATA

--- branches/benchmarking/python_bm/stata_columbus.py
+++ branches/benchmarking/python_bm/stata_columbus.py
@@ -4,13 +4,27 @@ Script to replicate steps in ../stata_bm/columbus.do
 
 import pysal as ps
 import numpy as np
-import econometrics as spreg
+from econometrics.spError import GM_Error
+from econometrics.twosls_sp import BaseGM_Lag as GM_Lag
 
 w = ps.open('../../../trunk/econometrics/examples/columbus.gal').read()
 w.transform = 'r'
-
 db = ps.open('../../../trunk/econometrics/examples/columbus.dbf')
 
-crime = np.array(db.by_col('CRIME'))
-w_crime = ps.lag_spatial(w, crime)
+crime = np.array([db.by_col('CRIME')]).T
+inc = np.array([db.by_col('INC')]).T
+hoval = np.array([db.by_col('HOVAL')]).T
+#w_crime = ps.lag_spatial(w, crime)
 
+x = np.hstack((inc, crime))
+
+# OLS (matches both)
+#model = ps.spreg.ols.OLS(hoval, x)
+
+# GM Error (matches R, not STATA)
+#model = GM_Error(hoval, x, w)
+
+# 2SLS Lag (does not match R or STATA, which both match)
+model = GM_Lag(hoval, x, w, w_lags=2)
+
+print model.betas
