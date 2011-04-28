edf71c8f67c220c3ad8546504155f69a7f4ca85c
dani <dani@a2f3893c-e248-4646-8fd3-e29ca8a5c454>
2011-04-28 20:18:52 +0000

    R benchmarking testing

--- branches/benchmarking/r_bm/stata_columbus.r
+++ branches/benchmarking/r_bm/stata_columbus.r
@@ -1,15 +1,26 @@
 # Script to replicate columbus regression to check STATA
 library(spdep)
+library(sem)
+library(car)
 library(foreign)
 
 w <- read.gal('../../../trunk/econometrics/examples/columbus.gal')
 w <- nb2listw(w)
 dbf <- read.dbf('../../../trunk/econometrics/examples/columbus.dbf')
 
+# OLS
+#model <- lm(HOVAL ~ INC + CRIME, data=dbf)
+# White corrention
+#white.vars <- hccm(model)
+# HAC
+
+# STSLS
+#model <- tsls(HOVAL ~ INC + CRIME, ~ INC + DISCBD, data=dbf)
+
 # GM Error
-#model <- GMerrorsar(HOVAL ~ INC + CRIME, data=dbf, w, return_LL=TRUE)
+model <- GMerrorsar(HOVAL ~ INC + CRIME, data=dbf, w, return_LL=TRUE)
 
 # S2SLS
-model <- stsls(HOVAL ~ INC + CRIME, data=dbf, w)
+#model <- stsls(HOVAL ~ INC + CRIME, data=dbf, w)
 
-summary(model)
+print(summary(model))
