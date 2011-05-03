a697d0a65276987f4a8b8184dbb9e335aaa90d29
dani <dani@a2f3893c-e248-4646-8fd3-e29ca8a5c454>
2011-05-03 01:34:49 +0000

    Changes: * Omega matrix for GM_Endog_Hom finalized * Documentation for GM_Endog_Hom added * Benchmarking file to test GM_Endog_Hom against STATA included Main conclusions: * In terms of betas, we match STATA to the 2nd or 3rd decimal * Std. Devs. are very much off * Initial betas (first 2SLS) match perfectly * Initial lambda is off at the 3rd decimal

--- python_bm/stata_columbus.py
+++ python_bm/stata_columbus.py
@@ -4,7 +4,7 @@ Script to replicate steps in ../stata_bm/columbus.do
 
 import pysal as ps
 import numpy as np
-from econometrics.spError import GM_Error
+from econometrics.spError import GM_Error, BaseGM_Endog_Error_Hom
 from econometrics.twosls_sp import BaseGM_Lag as GM_Lag
 
 w = ps.open('../../../trunk/econometrics/examples/columbus.gal').read()
@@ -14,6 +14,7 @@ db = ps.open('../../../trunk/econometrics/examples/columbus.dbf')
 crime = np.array([db.by_col('CRIME')]).T
 inc = np.array([db.by_col('INC')]).T
 hoval = np.array([db.by_col('HOVAL')]).T
+discbd = np.array([db.by_col('DISCBD')]).T
 #w_crime = ps.lag_spatial(w, crime)
 
 x = np.hstack((inc, crime))
@@ -22,9 +23,22 @@ x = np.hstack((inc, crime))
 #model = ps.spreg.ols.OLS(hoval, x)
 
 # GM Error (matches R, not STATA)
-#model = GM_Error(hoval, x, w)
+#model = GM_Error(hoval, x, w) # This model is not implemented in STATA's 'spivreg'
+
+# GM_hom
+model = BaseGM_Endog_Error_Hom(hoval, inc, w, crime, discbd)
 
 # 2SLS Lag (does not match R or STATA, which both match)
-model = GM_Lag(hoval, x, w, w_lags=2)
+#model = GM_Lag(hoval, x, w, w_lags=2)
 
+print '##### Betas #####'
 print model.betas
+print '### Std Devs ###'
+print map(np.sqrt, model.vm.diagonal())
+print '### VC Matrix Omega ###'
+print model.vm
+print '### Initial estimate of lambda ###'
+print model.lambda1
+print '### Initial estimate of betas ###'
+print model.tsls.betas
+
