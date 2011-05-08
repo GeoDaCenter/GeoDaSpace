f77c2d1c3d0f11b55aedb3e5dbac1275acd92181
dani <dani@a2f3893c-e248-4646-8fd3-e29ca8a5c454>
2011-05-08 17:33:37 +0000

    New tests for R (from the other day, forgot to check in)

--- branches/benchmarking/r_bm/stata_columbus.r
+++ branches/benchmarking/r_bm/stata_columbus.r
@@ -1,26 +1,46 @@
 # Script to replicate columbus regression to check STATA
 library(spdep)
+library(sphet)
 library(sem)
 library(car)
 library(foreign)
+
+##################### COLUMBUS #####################
 
 w <- read.gal('../../../trunk/econometrics/examples/columbus.gal')
 w <- nb2listw(w)
 dbf <- read.dbf('../../../trunk/econometrics/examples/columbus.dbf')
 
-# OLS
+##### OLS
 #model <- lm(HOVAL ~ INC + CRIME, data=dbf)
 # White corrention
 #white.vars <- hccm(model)
-# HAC
 
-# STSLS
+##### STSLS
 #model <- tsls(HOVAL ~ INC + CRIME, ~ INC + DISCBD, data=dbf)
 
-# GM Error
-model <- GMerrorsar(HOVAL ~ INC + CRIME, data=dbf, w, return_LL=TRUE)
+##### GM Error
+#model <- GMerrorsar(HOVAL ~ INC + CRIME, data=dbf, w, return_LL=TRUE)
 
-# S2SLS
+##### S2SLS
 #model <- stsls(HOVAL ~ INC + CRIME, data=dbf, w)
+# White
+#model <- stsls(HOVAL ~ INC + CRIME, data=dbf, w, robust=TRUE)
+
+##### SpHet Error
+model <- gstslshet(HOVAL ~ INC + CRIME, data=dbf, w, sarar=FALSE, inverse=FALSE)
+
+##### SpHet SARAR
+#model <- gstslshet(HOVAL ~ INC + CRIME, data=dbf, w, sarar=TRUE)
+
+
+##################### NAT #####################
+
+w <- read.gal('../../../trunk/econometrics/examples/NAT.gwt')
+w <- nb2listw(w)
+dbf <- read.dbf('../../../trunk/econometrics/examples/NAT.dbf')
+
+# STSLS-HAC
+stslshac(HR90 ~ RD90 + DV90, data=dbf, w)
 
 print(summary(model))
