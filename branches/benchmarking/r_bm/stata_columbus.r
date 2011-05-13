0bcda6e2192754c7ec7c38e504b1fda1381a9133
dani <dani@a2f3893c-e248-4646-8fd3-e29ca8a5c454>
2011-05-13 01:26:01 +0000

    minor changes to test files

--- branches/benchmarking/r_bm/stata_columbus.r
+++ branches/benchmarking/r_bm/stata_columbus.r
@@ -1,9 +1,28 @@
 # Script to replicate columbus regression to check STATA
 library(spdep)
-library(sphet)
+#library(sphet)
+
 library(sem)
-library(car)
+#library(car)
 library(foreign)
+
+setwd("~/code/spreg/branches/benchmarking/r_bm")
+
+source("~/code/sphet/R/dist_functions.R")
+source("~/code/sphet/R/distance.R")
+source("~/code/sphet/R/gs2slshet_bak.R")
+source("~/code/sphet/R/gwt2dist.R")
+source("~/code/sphet/R/kernelsfun.R")
+source("~/code/sphet/R/listw2dgCMatrix.R")
+source("~/code/sphet/R/Omega.R")
+source("~/code/sphet/R/s2slshac.R")
+source("~/code/sphet/R/summary.sphet.R")
+source("~/code/sphet/R/twostagels.R")
+source("~/code/sphet/R/utilities.R")
+
+
+
+
 
 ##################### COLUMBUS #####################
 
@@ -36,11 +55,11 @@ model <- gstslshet(HOVAL ~ INC + CRIME, data=dbf, w, sarar=FALSE, inverse=FALSE)
 
 ##################### NAT #####################
 
-w <- read.gal('../../../trunk/econometrics/examples/NAT.gwt')
-w <- nb2listw(w)
-dbf <- read.dbf('../../../trunk/econometrics/examples/NAT.dbf')
+#w <- read.gal('../../../trunk/econometrics/examples/NAT.gwt')
+#w <- nb2listw(w)
+#dbf <- read.dbf('../../../trunk/econometrics/examples/NAT.dbf')
 
 # STSLS-HAC
-stslshac(HR90 ~ RD90 + DV90, data=dbf, w)
+#stslshac(HR90 ~ RD90 + DV90, data=dbf, w)
 
 print(summary(model))
