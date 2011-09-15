b5d78e11c5c6b7afbefb5bf4c7594a1b8512c410
charlie <charlie@a2f3893c-e248-4646-8fd3-e29ca8a5c454>
2011-09-15 21:15:52 +0000

    initial control

--- trunk/geodaspace/prefrences/control.py
+++ trunk/geodaspace/prefrences/control.py
@@ -6,6 +6,12 @@ from geodaspace import remapEvtsToDispatcher#,DEBUG
 import preferences_xrc
 
 DEBUG = True
+
+STD_DEV_PAGE = 0
+GMM_PAGE = 1
+INSTRUMENTS_PAGE = 2
+OUTPUT_PAGE = 3
+OTHER_PAGE = 4
 class preferencesFrame(preferences_xrc.xrcgsPrefsFrame):
     """
     GeoDaSpace Preference Dialog -- Displays a Dialog for editing GeoDaSpace Preferences
@@ -27,10 +33,11 @@ class preferencesFrame(preferences_xrc.xrcgsPrefsFrame):
     {'stddev':{'ols':True,'2sls':False...
     """
     def __init__(self,parent=None):
-        remapEvtsToDispatcher(self, self.evtDistpatch)
+        remapEvtsToDispatcher(self, self.evtDispatch)
         preferences_xrc.xrcgsPrefsFrame.__init__(self,parent)
 
         self.dispatch = d = {}
+        d['saveButton'] = self.save
 
     def evtDispatch(self,evtName, evt):
         evtName,widgetName = evtName.rsplit('_',1)
@@ -38,4 +45,48 @@ class preferencesFrame(preferences_xrc.xrcgsPrefsFrame):
             self.dispatch[widgetName](evtName,evt)
         else:
             if DEBUG: print "not implemented:", evtName, widgetName
+
+    def error(self, msg, tagline="An Error has occurred"):
+        """ Display an error message to the user """
+        wx.MessageDialog(self, msg, tagline, style=wx.OK|wx.ICON_ERROR).ShowModal()
+        return
+        
+    def cancel(self, evtName=None, evt=None, value=None):
+        print "hide dialog"
+        return wx.ID_CANCEL
+    def save(self, evtName=None, evt=None, value=None):
+        #validation section, local names used in settings dict below.
+        try:
+            epsilon = float(self.StoppingCriterion.GetValue())
+            if epsilon >= 1.0:
+                raise ValueError
+        except:
+            self.prefNoteBook.SetSelection(GMM_PAGE)
+            self.StoppingCriterion.SetFocus()
+            self.error("Stopping Criterion must be a number < 1.0")
+            return
+
+        print evtName, evt, value
+        print "OLS  stddev.:", self.OLSNk.GetValue(), self.OLSN.GetValue()
+        print "2SLS stddev.:", self.twoSLSNk.GetValue(), self.twoSLSN.GetValue()
+        print "GMlg stddev.:", self.GMlagNk.GetValue(), self.GMlagN.GetValue()
+        print "Othr stddev.:", False, self.othermodelsN.GetValue()
+        s = {}
+        s['sig2n_k'] = { 'ols':self.OLSNk.GetValue(),
+                        '2sls':self.twoSLSNk.GetValue(),
+                        'gmlag':self.GMlagNk.GetValue(),
+                        'other':(not self.othermodelsN.GetValue()) }
+        s['gmm'] = {'max_iter':self.MaxIterations.GetValue(),
+                    'epsilon': epsilon,
+                    'inferenceOnLambda': self.inferenceOnLambda.GetValue(),
+                    'inv_method': {0:'power_exp',1:'true_inverse'}[self.CompInverse.GetSelection()],
+                    'step1c': self.Step1c.GetValue()
+                   }
+        s['instruments'] = {'w_lags':self.NumSpatialLags.GetValue(), 'lag_q':self.IncludeLagsofUserInst.GetValue()}
+        s['output'] = {'vm_summary':self.ShowVarCovarMatrix.GetValue(), 'save_pred_residuals':saelf.saveValuesResiduals.GetValue()}
+        s['other'] = {'ols_diagnostics':self.OLSdiagnostics.GetValue(), 'residualMoran',self.residualMoran.GetValue()}
+        self._prefs = s
+        print "hide dialog"
+        return wx.ID_OK
+            
 
