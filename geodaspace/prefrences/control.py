1bf66249a25da9eb28988e6fa789d128ce679fd3
charlie <charlie@a2f3893c-e248-4646-8fd3-e29ca8a5c454>
2011-09-19 21:29:29 +0000

    setters and getters for GeoDaSpace Preferences

--- geodaspace/prefrences/control.py
+++ geodaspace/prefrences/control.py
@@ -32,15 +32,35 @@ class preferencesFrame(preferences_xrc.xrcgsPrefsFrame):
     Settings Dictionary Format,
     {'stddev':{'ols':True,'2sls':False...
     """
+    DEFAULTS = {
+        'sig2n_k': {'other': False, 'ols': True, 'gmlag': False, '2sls': True},
+        'gmm': {'epsilon': 1e-05, 'inferenceOnLambda': True, 'max_iter': 1, 'step1c': False, 'inv_method': 'power_exp'},
+        'instruments': {'lag_q': True, 'w_lags': 1},
+        'other': {'ols_diagnostics': True, 'numcores': 1, 'residualMoran': False},
+        'output': {'save_pred_residuals': False, 'vm_summary': False}}
+
     def __init__(self,parent=None):
         remapEvtsToDispatcher(self, self.evtDispatch)
         preferences_xrc.xrcgsPrefsFrame.__init__(self,parent)
 
         self.dispatch = d = {}
         d['saveButton'] = self.save
+        self.SetPrefs(self.DEFAULTS)
+
+    def __set_modified(self,val):
+        self.__mod = bool(val)
+        if self.__mod:
+            self.SetTitle("GeoDaSpace Preferences*")
+        else:
+            self.SetTitle("GeoDaSpace Preferences")
+    def __get_modified(self):
+        return self.__mod
+    modified = property(fget = __get_modified, fset = __set_modified)
 
     def evtDispatch(self,evtName, evt):
         evtName,widgetName = evtName.rsplit('_',1)
+        if evtName not in ['restoreButton','saveButton','cancelButton']:
+            self.modified = True
         if widgetName in self.dispatch:
             self.dispatch[widgetName](evtName,evt)
         else:
@@ -55,6 +75,31 @@ class preferencesFrame(preferences_xrc.xrcgsPrefsFrame):
         print "hide dialog"
         return wx.ID_CANCEL
     def save(self, evtName=None, evt=None, value=None):
+        print evtName, evt, value
+        #print "OLS  stddev.:", self.OLSNk.GetValue(), self.OLSN.GetValue()
+        #print "2SLS stddev.:", self.twoSLSNk.GetValue(), self.twoSLSN.GetValue()
+        #print "GMlg stddev.:", self.GMlagNk.GetValue(), self.GMlagN.GetValue()
+        #print "Othr stddev.:", False, self.othermodelsN.GetValue()
+        print self.GetPrefs()
+        print "hide dialog"
+        return wx.ID_OK
+    def SetPrefs(self,prefs):
+        if 'sig2n_k' in prefs:
+            sig = prefs['sig2n_k']
+            # No option for other models.
+            self.othermodelsN.SetValue(True)
+            if 'ols' in sig:
+                self.OLSNk.SetValue(bool(sig['ols']))
+                self.OLSN.SetValue(not bool(sig['ols']))
+            if '2sls' in sig:
+                print sig['2sls']
+                self.twoSLSNk.SetValue(bool(sig['2sls']))
+                #self.twoSLSN.SetValue(not bool(sig['2sls']))
+            if 'gmlag' in sig:
+                self.twoSLSNk.SetValue(bool(sig['gmlag']))
+                self.twoSLSN.SetValue(not bool(sig['gmlag']))
+        
+    def GetPrefs(self):
         #validation section, local names used in settings dict below.
         try:
             epsilon = float(self.StoppingCriterion.GetValue())
@@ -66,11 +111,6 @@ class preferencesFrame(preferences_xrc.xrcgsPrefsFrame):
             self.error("Stopping Criterion must be a number < 1.0")
             return
 
-        print evtName, evt, value
-        print "OLS  stddev.:", self.OLSNk.GetValue(), self.OLSN.GetValue()
-        print "2SLS stddev.:", self.twoSLSNk.GetValue(), self.twoSLSN.GetValue()
-        print "GMlg stddev.:", self.GMlagNk.GetValue(), self.GMlagN.GetValue()
-        print "Othr stddev.:", False, self.othermodelsN.GetValue()
         s = {}
         s['sig2n_k'] = { 'ols':self.OLSNk.GetValue(),
                         '2sls':self.twoSLSNk.GetValue(),
@@ -83,10 +123,10 @@ class preferencesFrame(preferences_xrc.xrcgsPrefsFrame):
                     'step1c': self.Step1c.GetValue()
                    }
         s['instruments'] = {'w_lags':self.NumSpatialLags.GetValue(), 'lag_q':self.IncludeLagsofUserInst.GetValue()}
-        s['output'] = {'vm_summary':self.ShowVarCovarMatrix.GetValue(), 'save_pred_residuals':saelf.saveValuesResiduals.GetValue()}
-        s['other'] = {'ols_diagnostics':self.OLSdiagnostics.GetValue(), 'residualMoran',self.residualMoran.GetValue()}
+        s['output'] = {'vm_summary':self.ShowVarCovarMatrix.GetValue(), 'save_pred_residuals':self.saveValuesResiduals.GetValue()}
+        s['other'] = {'ols_diagnostics':self.OLSdiagnostics.GetValue(), 'residualMoran':self.residualMoran.GetValue()}
+        s['other']['numcores'] = self.numcores.GetValue()
         self._prefs = s
-        print "hide dialog"
-        return wx.ID_OK
+        return s
             
 
