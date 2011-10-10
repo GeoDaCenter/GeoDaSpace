a9339109dcc21a002dbf54a61844453495415e56
nick <nick@a2f3893c-e248-4646-8fd3-e29ca8a5c454>
2011-10-10 23:38:34 +0000

    Adding pseudo r^2 for TSLS and STSLS to diagnostics.

--- trunk/econometrics/diagnostics_tsls.py
+++ trunk/econometrics/diagnostics_tsls.py
@@ -5,8 +5,10 @@ Diagnostics for two stage least squares regression estimations.
 
 import pysal
 from pysal.common import *
+from scipy.stats import pearsonr
 from math import sqrt
 from twosls import BaseTSLS as TSLS
+from twosls_sp import GM_Lag
 from pysal.spreg.ols import BaseOLS as OLS
 
 
@@ -142,6 +144,105 @@ def t_stat(reg, z_stat=False):
         else:
             ts_result.append((t, stats.t.sf(abs(t),n-k)*2))
     return ts_result
+
+
+def pr2_aspatial(tslsreg):
+    """
+    Calculates the pseudo r^2 for the two stage least squares regression.
+    
+    Parameters
+    ----------
+    tslsreg             : two stage least squares regression object
+                          output instance from a two stage least squares
+                          regression model
+
+        
+    Returns
+    -------    
+    pr2_result          : float
+                          value of the squared pearson correlation between
+                          the y and tsls-predicted y vectors
+
+    
+    Examples
+    --------
+
+    >>> import numpy as np
+    >>> import pysal
+    >>> db=pysal.open("examples/columbus.dbf","r")
+    >>> y = np.array(db.by_col("CRIME"))
+    >>> y = np.reshape(y, (49,1))
+    >>> X = []
+    >>> X.append(db.by_col("INC"))
+    >>> X = np.array(X).T
+    >>> yd = []
+    >>> yd.append(db.by_col("HOVAL"))
+    >>> yd = np.array(yd).T
+    >>> q = []
+    >>> q.append(db.by_col("DISCBD"))
+    >>> q = np.array(q).T
+    >>> reg = TSLS(y, X, yd, q=q)
+    >>> result = pr2_aspatial(reg)
+    >>> print("%1.6f"%result)    
+    0.279361
+
+    """
+
+    y = tslsreg.y
+    predy = tslsreg.predy
+    pr = pearsonr(y,predy)[0]
+    pr2_result = float(pr**2)
+    return pr2_result
+
+
+def pr2_spatial(tslsreg):
+    """
+    Calculates the pseudo r^2 for the spatial two stage least squares 
+    regression.
+    
+    Parameters
+    ----------
+    stslsreg            : spatial two stage least squares regression object
+                          output instance from a spatial two stage least 
+                          squares regression model
+
+        
+    Returns
+    -------    
+    pr2_result          : float
+                          value of the squared pearson correlation between
+                          the y and stsls-predicted y vectors
+
+    
+    Examples
+    --------
+
+    >>> import numpy as np
+    >>> import pysal
+    >>> import pysal.spreg.diagnostics as D
+    >>> w = pysal.rook_from_shapefile("examples/columbus.shp")
+    >>> w.transform = 'r'
+    >>> db=pysal.open("examples/columbus.dbf","r")
+    >>> y = np.array(db.by_col("HOVAL"))
+    >>> y = np.reshape(y, (49,1))
+    >>> X = np.array(db.by_col("INC"))
+    >>> X = np.reshape(X, (49,1))
+    >>> yd = np.array(db.by_col("CRIME"))
+    >>> yd = np.reshape(yd, (49,1))
+    >>> q = np.array(db.by_col("DISCBD"))
+    >>> q = np.reshape(q, (49,1))
+    >>> reg = GM_Lag(y, X, w=w, yend=yd, q=q, w_lags=2, name_x=['inc'], name_y='hoval', name_yend=['crime'], name_q=['discbd'], name_ds='columbus')
+    >>> result = pr2_spatial(reg)
+    >>> print("%1.6f"%result)
+    0.299649
+
+    """
+
+    y = tslsreg.y
+    predy_sp = tslsreg.predy_sp
+    pr = pearsonr(y,predy_sp)[0]
+    pr2_result = float(pr**2)
+    return pr2_result
 
 
 
