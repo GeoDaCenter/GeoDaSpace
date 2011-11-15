f94f0bb3cb2bc553c12b8bcf5173ce148b1e91da
dani <dani@a2f3893c-e248-4646-8fd3-e29ca8a5c454>
2011-11-15 03:08:54 +0000

    Changes to check behaviour when dummies included

--- branches/benchmarking/python_bm/stata_columbus.py
+++ branches/benchmarking/python_bm/stata_columbus.py
@@ -9,29 +9,34 @@ import numpy as np
 from econometrics.twosls_sp import BaseGM_Lag as GM_Lag
 #from econometrics.spError import BaseGM_Combo as GM_Combo
 from econometrics.error_sp_het import BaseGM_Error_Het as BaseGM_Error_Het
+from econometrics.error_sp_hom import BaseGM_Endog_Error_Hom as BaseGM_Endog_Error_Hom
 from econometrics.error_sp_het import BaseGM_Endog_Error_Het as BaseGM_Endog_Error_Het
 from econometrics.error_sp_het import BaseGM_Combo_Het as BaseGM_Combo_Het
+from econometrics.error_sp_hom import BaseGM_Combo_Hom as BaseGM_Combo_Hom
+from econometrics.twosls import TSLS
 
-#w = ps.open('../../../trunk/econometrics/examples/columbus.gal').read()
-w = ps.open('C:/Users/Pedro/Documents/Academico/GeodaCenter/python/SVN/spreg/trunk/econometrics/examples/columbus.gal').read()
+w = ps.open('../../../trunk/econometrics/examples/columbus.gal').read()
+#w = ps.open('C:/Users/Pedro/Documents/Academico/GeodaCenter/python/SVN/spreg/trunk/econometrics/examples/columbus.gal').read()
 w.transform = 'r'
-#db = ps.open('../../../trunk/econometrics/examples/columbus.dbf')
-db = ps.open('C:/Users/Pedro/Documents/Academico/GeodaCenter/python/SVN/spreg/trunk/econometrics/examples/columbus.dbf')
+db = ps.open('../../../trunk/econometrics/examples/columbus.dbf')
+#db = ps.open('C:/Users/Pedro/Documents/Academico/GeodaCenter/python/SVN/spreg/trunk/econometrics/examples/columbus.dbf')
 
 crime = np.array([db.by_col('CRIME')]).T
 inc = np.array([db.by_col('INC')]).T
 hoval = np.array([db.by_col('HOVAL')]).T
 discbd = np.array([db.by_col('DISCBD')]).T
+nsa = np.array([db.by_col('NSA')]).T
 #w_crime = ps.lag_spatial(w, crime)
 
-x = np.hstack((hoval, inc))
+x = np.hstack((inc, nsa))
 
 # OLS (matches both)
 #model = ps.spreg.ols.OLS(crime, x)
 
 
 # 2SLS Lag 
-#model = GM_Lag(crime, x, w, w_lags=2)
+model = TSLS(hoval, inc, yend=crime, q=discbd, sig2n_k=True)
+#model = GM_Lag(hoval, x, w=w, w_lags=2)
 #model = GM_Lag(crime, x, w, w_lags=2, robust="White")
 #model = GM_Lag(crime, inc, yend=hoval, q=discbd, w=w, w_lags=2)
 #model = GM_Lag(crime, inc, yend=hoval, q=discbd, w=w, w_lags=2, robust="White")
@@ -48,14 +53,16 @@ x = np.hstack((hoval, inc))
 # GM Error Hom
 #model = BaseGM_Endog_Error_Hom(crime, inc, w, hoval, discbd)
 # GM Combo Hom
-#model = BaseGM_Endog_Combo_Hom(crime, inc, w, hoval, discbd)
-
+#model = BaseGM_Combo_Hom(hoval, x, w=w, w_lags=2)
+#model = BaseGM_Combo_Hom(hoval, x, w=w, w_lags=2)
+#model = BaseGM_Endog_Error_Hom(hoval, x, yend=crime, q=discbd, w=w,\
+#        A1='hom_sc')
 
 
 # GM Error Het
 #model = BaseGM_Error_Het(crime, x, w) #to match Stata the following code is required:
-ones = np.ones(crime.shape)
-model = BaseGM_Endog_Error_Het(crime, ones, x, x, w, step1c=False, constant=False)
+#ones = np.ones(crime.shape)
+#model = BaseGM_Endog_Error_Het(crime, ones, x, x, w, step1c=False, constant=False)
 '''
 self.x = reg.z
 self.y = reg.y
@@ -69,18 +76,24 @@ self._cache = {}
 # GM Error Het with user-defined endog
 #model = BaseGM_Endog_Error_Het(crime, inc, hoval, discbd, w, step1c=False)
 # GM Combo Het
-#model = BaseGM_Combo_Het(crime, x, w=w, w_lags=2, step1c=False)
+#model = BaseGM_Combo_Het(hoval, x, w=w, w_lags=2, step1c=False)
 # GM Combo Het with user-defined endog
-#model = BaseGM_Combo_Het(crime, inc, w=w, yend=hoval, q=discbd, w_lags=2, step1c=False)
+#model = BaseGM_Combo_Het(crime, x, w=w, yend=hoval, q=discbd, w_lags=2, step1c=False)
+
+# GM Endog Error Het
+#model = BaseGM_Endog_Error_Het(hoval, inc, w, crime, discbd)
 
 
-print '##### Betas #####'
-print model.betas
+#print '##### Betas #####'
+#print model.betas
 #   print '### Std Devs ###'
-print map(np.sqrt, model.vm.diagonal())
+#print map(np.sqrt, model.vm.diagonal())
+
+for i,j in zip(model.betas, map(np.sqrt, model.vm.diagonal())):
+    print i, '\t', j
 #print map(np.sqrt, model.vm.diagonal() * model.n/(model.n-model.k))
-#   print '### VC Matrix Omega ###'
-#   print model.vm
+#print '### VC Matrix Omega ###'
+#print model.vm
 #print '### Initial estimate of lambda ###'
 #print model.lambda1
 #   print '### Initial estimate of betas ###'
