a7e9e7c5bf72ebb4e1d3e7e008e1ef8a7cbe658e
dani <dani@a2f3893c-e248-4646-8fd3-e29ca8a5c454>
2011-11-30 23:17:25 +0000

    Enchancements to replicate large data simulations

--- branches/benchmarking/python_bm/large_sim_data.py
+++ branches/benchmarking/python_bm/large_sim_data.py
@@ -5,13 +5,14 @@ Script to create large datasets and benchmark times
 """
 
 import pysal as ps
-import time
+import time, sys
 import numpy as np
 from pysal.spreg.ols import OLS
 from pysal.spreg.ols import BaseOLS
 #from opt_diagnostics_sp import LMtests, MoranRes
 from pysal.spreg.diagnostics_sp import LMtests, MoranRes
 from econometrics.error_sp import BaseGM_Error, GM_Error
+from econometrics.error_sp_hom import BaseGM_Error_Hom, BaseGM_Combo_Hom, GM_Error_Hom, GM_Combo_Hom
 from econometrics.error_sp_het import BaseGM_Error_Het, BaseGM_Combo_Het, GM_Error_Het, GM_Combo_Het
 from econometrics.twosls_sp import BaseGM_Lag, GM_Lag
 #from econometrics.testing_utils import Test_Data as Data
@@ -53,7 +54,9 @@ def test_large_olsSPd(s, k, log=None, base=False, sw=False, a=True):
 
     ti = time.time()
     t0 = time.time()
-    data = Data(s**2, k, 'large', '../../../trunk/econometrics/', omit_w=True)
+    data = Model_Inputs(s**2, intercept=False, x_means=[0.]*(k+1),
+            x_std_devs=[1.]*(k+1), xs=(k+1)) 
+    y, x = data.x[:, 0:1], data.x[:, 1:]
     t1 = time.time()
     tf = t1 - t0
     creDa = 'Create data:\t\t\t%.5f seconds\n'%tf
@@ -84,10 +87,10 @@ def test_large_olsSPd(s, k, log=None, base=False, sw=False, a=True):
     t0 = time.time()
     if base:
         print 'Running barebone version of OLS'
-        ols = BaseOLS(data.y, data.x)
+        ols = BaseOLS(y, x)
     else:
         print 'Running full end-user version of OLS'
-        ols = OLS(data.y, data.x)
+        ols = OLS(y, x)
     t1 = time.time()
     tf = t1 - t0
     runOls = 'Regression:\t\t\t%.5f seconds\n'%tf
@@ -123,7 +126,7 @@ def test_large_olsSPd(s, k, log=None, base=False, sw=False, a=True):
         log.close()
     return [ols, lms, moran]
 
-def test_large_GMSWLS(s, k, log=None, base=False, sw=False, a=True):
+def test_large_GMhom(s, k, log=None, base=False, sw=False, a=True):
     """
     Run and time GMSWLS 
     ...
@@ -197,10 +200,105 @@ def test_large_GMSWLS(s, k, log=None, base=False, sw=False, a=True):
     t0 = time.time()
     if base:
         print 'Running barebone version of GM_Error'
-        gmswls = BaseGM_Error(y, x, w)
+        gmswls = BaseGM_Error_Hom(y, x, w)
     else:
         print 'Running full end-user version of GM_Error'
-        gmswls = GM_Error(y, x, w)
+        gmswls = GM_Error_Hom(y, x, w)
+    t1 = time.time()
+    tf = t1 - t0
+    runGmswls = 'Regression:\t\t\t%.5f seconds\n'%tf
+    if a:
+        log.write(runGmswls)
+    print runGmswls
+
+    tff=time.time()
+    tt = tff - ti
+    total = 'Total final time:\t\t%.5f seconds\n'%tt
+    if a:
+        log.write(total)
+    print total
+    if a:
+        log.close()
+    return gmswls
+
+def test_large_GMhet(s, k, log=None, base=False, sw=False, a=True):
+    """
+    Run and time GMSWLS 
+    ...
+
+    Arguments
+    ---------
+    s       : int
+              Side of the lattice from which to build the weights on
+              n = s**2
+    k       : int
+              N of variables to include in regression
+    log     : string
+              Path of log file to append results to
+    a       : boolean
+              Switcher to append log to existing file or just print to
+              terminal
+
+    Returns
+    -------
+              Updates on-the-fly the log file with new timing results
+    """
+    if not log:
+        a = False
+        print 'Not appending to any file'
+    if a:
+        log = open(log, 'a')
+    n = 'n: %i\n'%s**2
+    if a:
+        log.write(n)
+    model = 'Model: GMSWLS'
+    if a:
+        log.write(model)
+    print n
+    vars = 'k: %i\n'%k
+    if a:
+        log.write(vars)
+    print vars
+
+    ti = time.time()
+    t0 = time.time()
+    data = Model_Inputs(s**2, intercept=False, x_means=[0.]*(k+1),
+            x_std_devs=[1.]*(k+1), xs=(k+1)) 
+    y, x = data.x[:, 0:1], data.x[:, 1:]
+    t1 = time.time()
+    tf = t1 - t0
+    creDa = 'Create data:\t\t\t%.5f seconds\n'%tf
+    if a:
+        log.write(creDa)
+    print creDa
+
+    t0 = time.time()
+    if sw:
+        w = ps.weights.lat2SW(s, s, criterion='queen')
+        w.n = s**2
+        w.sparse = w
+        w.s0 = np.sum(w.data)
+
+        t = w.T + w
+        t2 = t.multiply(t) # element-wise square
+        w.s1 = t2.sum()/2.
+
+    else:
+        w = ps.lat2W(s, s, rook=False)
+    t1 = time.time()
+    tf = t1 - t0
+    creWe = 'Created Weights:\t\t%.5f seconds\n'%tf
+    if a:
+        log.write(creWe)
+    print creWe
+
+    t0 = time.time()
+    if base:
+        print 'Running barebone version of GM_Error'
+        gmswls = BaseGM_Error_Het(y, x, w)
+    else:
+        print 'Running full end-user version of GM_Error'
+        gmswls = GM_Error_Het(y, x, w)
     t1 = time.time()
     tf = t1 - t0
     runGmswls = 'Regression:\t\t\t%.5f seconds\n'%tf
@@ -719,13 +817,13 @@ def test_small_all(s, k, log=None, base=False, sw=False, a=True):
     t0 = time.time()
     if base:
         print 'Running barebone version of GM_Error'
-        gmswls = BaseGM_Error(y, x, w)
+        gmswls = BaseGM_Error_Hom(y, x, w)
     else:
         print 'Running full end-user version of GM_Error'
-        gmswls = GM_Error(y, x, w)
+        gmswls = GM_Error_Hom(y, x, w)
     t1 = time.time()
     tf = t1 - t0
-    runGmswls = 'GM_Error:\t\t\t%.5f seconds\n'%tf
+    runGmswls = 'GM_Error_Hom:\t\t\t%.5f seconds\n'%tf
     if a:
         log.write(runGmswls)
     print runGmswls
@@ -783,22 +881,30 @@ def test_small_all(s, k, log=None, base=False, sw=False, a=True):
     return gstsls_het
 
 sizes = [150, 300, 450, 600, 750, 800, 850, 900, 1000]
-#sizes = [150]
-#sizes = [500, 750, 1000, 1150, 1300, 1450, 1600]
-#sizes = [1750, 1900, 2000, 2050, 2100]
-#sizes = [1750]
-#sizes = [30, 50, 70, 100]
-#sizes = [100]
+sizes = [100]
 
+method, side = sys.argv[-2: ]
+
+methods = {'ols': test_large_olsSPd,
+        'lag': test_large_STSLS,
+        'error_hom': test_large_GMhom,
+        'error_het': test_large_GMhet}
+        
+logfile = '/Users/dani/Desktop/python_%s.log'%method
+model = methods[method](int(side), 100, log=logfile, a=True, base=True,
+        sw=True)
+
+'''
 for side in sizes:
    #ols_models = test_large_olsSPd(side, 10, log='/Users/dani/Dropbox/aagLogs/ols_py.log', a=True, base=True, sw=False)
    #gmswls = test_large_GMSWLS(side, 10, log='/Users/dani/Dropbox/aagLogs/gmswls_py.log', a=True, base=True, sw=False)
-    stsls = test_large_STSLS(side, 10, log='/Users/dani/Dropbox/aagLogs/stsls_py.log', a=False, base=True, sw=True)
+    stsls = test_large_STSLS(side, 10, log='/Users/dani/Desktop/stsls_py.log', a=True, base=True, sw=True)
    #sp_models = test_large_spHet_error_models(side, 10,
    #        log='/Users/dani/Dropbox/aagLogs/spHet_error_py.log', a=True, base=True, sw=False)
    #sp_models = test_large_spHet_sarar_models(side, 10,
    #        log='/Users/dani/Dropbox/aagLogs/spHet_sarar_py.log', a=True, base=True, sw=False)
    #smAll = test_small_all(side, 10, base=True)
    #smAll = test_large_spHet_error_models(side, 10, base=True, sw=True)
+'''
 
 
