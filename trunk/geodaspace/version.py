ef2b82ef0cc73f0085ee1d69170f5dd6312520f5
charlie <charlie@a2f3893c-e248-4646-8fd3-e29ca8a5c454>
2012-01-09 15:22:37 +0000

    Small change to build script to name OSX and Windows builds properly updating version info for 0.7.3

--- trunk/geodaspace/version.py
+++ trunk/geodaspace/version.py
@@ -15,11 +15,14 @@ def get_long_version():
 #version = "0.7.0" #spreg r682, pysal r1069
 #version_type = "alpha"
 #version_date = datetime.date(2011,12,14)
-
 #version = "0.7.1" #spreg r683, pysal r1070
 #version_type = "alpha"
 #version_date = datetime.date(2011,12,15)
 #Current Version
-version = "0.7.2" #spreg r683, pysal r1070
+#version = "0.7.2" #spreg r683, pysal r1070
+#version_type = "alpha"
+#version_date = datetime.date(2011,12,23)
+
+version = "0.7.3" #spreg r688, pysal r1070
 version_type = "alpha"
-version_date = datetime.date(2011,12,23)
+version_date = datetime.date(2012, 1, 9)
