9ef8ff02316136a786eff8597cec717ec812b979
stephens <stephens@a2f3893c-e248-4646-8fd3-e29ca8a5c454>
2013-01-18 18:57:37 +0000

    - removed whitespace in binary filenames - added png version of icon - increased resolution of apple icon

--- build_geodaspace.py
+++ build_geodaspace.py
@@ -52,7 +52,7 @@ if sys.platform == 'darwin':
            name='GeoDaSpace',
            version=geodaspace.version.version,
          )
-    os.rename('dist/GeoDaSpace.app','dist/GeoDaSpace OSX %s.app'%geodaspace.version.version)
+    os.rename('dist/GeoDaSpace.app','dist/GeoDaSpace_OSX_%s.app'%geodaspace.version.version)
 elif sys.platform == 'win32':
     import py2exe
     from glob import glob
@@ -72,4 +72,4 @@ elif sys.platform == 'win32':
            name='GeoDaSpace',
            version=geodaspace.version.version,
          )
-    os.rename('dist/GeoDaSpace.exe','dist/GeoDaSpace Windows %s.exe'%geodaspace.version.version)
+    os.rename('dist/GeoDaSpace.exe','dist/GeoDaSpace_Windows_%s.exe'%geodaspace.version.version)
