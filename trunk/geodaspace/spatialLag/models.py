ddc5e76fe0e3f5f483406c19a99e84e7714d3887
stephens <stephens@a2f3893c-e248-4646-8fd3-e29ca8a5c454>
2013-05-01 20:35:56 +0000

    Minor edits conforming to PEP8.

--- trunk/geodaspace/spatialLag/models.py
+++ trunk/geodaspace/spatialLag/models.py
@@ -7,13 +7,16 @@ import pysal
 DTYPE = 'listvars'
 FORMATHEADER = 0
 
+
 class M_CreateSpatialLag(AbstractModel):
-    DATA_KEYS_ORDER = ['vars','wtFiles','wtFile','dataFile']
+    DATA_KEYS_ORDER = ['vars', 'wtFiles', 'wtFile', 'dataFile']
+
     def __init__(self):
         AbstractModel.__init__(self)
-        #dispatch
-        self.dispatch = {'wtFile':self.__wtFile,'dataFile':self.__dataFile}
+        self.dispatch = {
+            'wtFile': self.__wtFile, 'dataFile': self.__dataFile}  # dispatch
         self.reset()
+
     def reset(self):
         self.data = {}
         self.data['wtFiles'] = ['']
@@ -21,115 +24,128 @@ class M_CreateSpatialLag(AbstractModel):
         self.data['vars'] = []
         self.newVars = []
         self.newVarNames = []
-    def addVar(self,var):
+
+    def addVar(self, var):
         self.newVars.append(var)
-        var.set('vars',self.get('vars'))
+        var.set('vars', self.get('vars'))
         var.addListener(self.varChanged)
-    def varChanged(self,varMdl):
+
+    def varChanged(self, varMdl):
         newVarNames = [var.get('newVarName') for var in self.newVars]
         if newVarNames is not self.newVarNames:
             for var in self.newVars:
                 if var.newVars == newVarNames:
                     pass
                 else:
-                    var.newVars=newVarNames
-                    var.update()
+                    var.newVars = newVarNames
+                    var.update()  # method from AbstractModel
             self.newVarNames = newVarNames
-    def get(self,key=None):
+
+    def get(self, key=None):
         if key:
             if key in self.data:
                 return self.data[key]
             else:
-                print "Model Error: %s, not in model"%key
+                print "Model Error: %s, not in model" % key
                 return None
         else:
             for key in self.data:
                 if not key in self.DATA_KEYS_ORDER:
-                    print "Model Warning: %s, not ORDERED"%key
+                    print "Model Warning: %s, not ORDERED" % key
                     self.DATA_KEYS_ORDER.append(key)
             for key in self.DATA_KEYS_ORDER:
                 if not key in self.data:
-                    print "Model Warning: %s, not in data"%key
+                    print "Model Warning: %s, not in data" % key
                     self.data[key] = None
             return self.data
-    def set(self,key=None,value=None,passive=False):
-        print "Setting",key,value
+
+    def set(self, key=None, value=None, passive=False):
+        print "Setting", key, value
         if key in self.dispatch:
             self.dispatch[key](value)
         else:
             self.data[key] = value
         if not passive:
-            self.update()
+            self.update()  # method from AbstractModel
+
     def verify(self):
         """ Verify the model before running """
         if self.data['dataFile']:
             return True
-    def run(self,path):
+
+    def run(self, path):
         if self.verify():
             print "running"
-            #print self.data['wtFiles'][self.data['wtFile']]
+            # print self.data['wtFiles'][self.data['wtFile']]
             newVars = [var.run() for var in self.newVars]
             names = [v[0] for v in newVars]
             vars = [v[1] for v in newVars]
             db = self.db()
-            xid = [ db.header.index(i) for i in vars ]
-            X = [db[:,i] for i in xid]
+            xid = [db.header.index(i) for i in vars]
+            X = [db[:, i] for i in xid]
             W = self.loadWeights()
 
-            new_header = db.header+names
-            new_spec = db.field_spec+[('N',20,10) for n in names]
+            new_header = db.header + names
+            new_spec = db.field_spec + [('N', 20, 10) for n in names]
             data = db.read()
             db.close()
-            newdb = pysal.open(path,'w')
+            newdb = pysal.open(path, 'w')
             newdb.header = new_header
             newdb.field_spec = new_spec
 
-            lag = [pysal.lag_spatial(W,y) for y in X]
-            lag = zip(*lag) #transpose
-            lag = map(list,lag)
-            for i,row in enumerate(data):
-                newdb.write(row+lag[i])
+            lag = [pysal.lag_spatial(W, y) for y in X]
+            lag = zip(*lag)  # transpose
+            lag = map(list, lag)
+            for i, row in enumerate(data):
+                newdb.write(row + lag[i])
             newdb.close()
+
     def db(self):
-        return pysal.open(self.data['dataFile'],'r')
+        return pysal.open(self.data['dataFile'], 'r')
+
     def loadWeights(self):
         wtFile = self.data['wtFiles'][self.data['wtFile']]
-        if issubclass(type(wtFile),basestring):
-            W = pysal.open(wtFile,'r').read()
-            W.transform = 'r' #see issue #138
+        if issubclass(type(wtFile), basestring):
+            W = pysal.open(wtFile, 'r').read()
+            W.transform = 'r'  # see issue #138
             return W
         else:
             W = wtFile.w
             if W.transform != 'r':
                 W.transform = 'r'
             return W
-    def __dataFile(self,value=None):
+
+    def __dataFile(self, value=None):
         if value is not None:
             if os.path.exists(value) and not os.path.isdir(value):
                 self.data['dataFile'] = value
                 db = self.db()
                 for v in self.newVars:
-                    v.set('vars',db.header)
-                self.set('vars',db.header)
+                    v.set('vars', db.header)
+                self.set('vars', db.header)
             else:
                 self.data['dataFile'] = False
-    def __wtFile(self,value=None):
+
+    def __wtFile(self, value=None):
         if value is not None:
-            if type(value) == int: #change the current wtFile from the GUI
+            if type(value) == int:  # change the current wtFile from the GUI
                 self.data['wtFile'] = value
-            else: #add str to list
+            else:  # add str to list
                 if not value in self.data['wtFiles']:
                     self.data['wtFiles'].append(value)
                 self.data['wtFile'] = self.data['wtFiles'].index(value)
 
+
 class M_spLagVariable(AbstractModel):
     """ Model for an XRC panel that contains, [textCtrl] = W*[dropDown] """
-    DATA_KEYS_ORDER = ['vars','var','newVarName','caution']
+    DATA_KEYS_ORDER = ['vars', 'var', 'newVarName', 'caution']
+
     def __init__(self):
         AbstractModel.__init__(self)
-        #dispatch
-        self.dispatch = {'vars':self.__vars}
+        # dispatch
+        self.dispatch = {'vars': self.__vars}
         self.reset()
+
     def reset(self):
         self.data = {}
         self.data['vars'] = ['']
@@ -138,35 +154,38 @@ class M_spLagVariable(AbstractModel):
         self.data['caution'] = False
         self.newVars = []
         self.varsChanged = True
+
     def run(self):
-        return self.data['newVarName'],self.data['vars'][self.data['var']]
-    def get(self,key=None):
+        return self.data['newVarName'], self.data['vars'][self.data['var']]
+
+    def get(self, key=None):
         if key:
             if key in self.data:
                 return self.data[key]
             else:
-                print "Model Error: %s, not in model"%key
+                print "Model Error: %s, not in model" % key
                 return None
         else:
             for key in self.data:
                 if not key in self.DATA_KEYS_ORDER:
-                    print "Model Warning: %s, not ORDERED"%key
+                    print "Model Warning: %s, not ORDERED" % key
                     self.DATA_KEYS_ORDER.append(key)
             for key in self.DATA_KEYS_ORDER:
                 if not key in self.data:
-                    print "Model Warning: %s, not in data"%key
+                    print "Model Warning: %s, not in data" % key
                     self.data[key] = None
             return self.data
-    def set(self,key=None,value=None,passive=False):
-        print "M_spLagVar Setting",key,value
+
+    def set(self, key=None, value=None, passive=False):
+        print "M_spLagVar Setting", key, value
         if key in self.dispatch:
             self.dispatch[key](value)
         else:
             self.data[key] = value
         if not passive:
-            self.update()
-    def __vars(self,value=None):
+            self.update()  # method from AbstractModel
+
+    def __vars(self, value=None):
         if value is not None:
             self.data['vars'] = value
-            self.varsChanged=True
-
+            self.varsChanged = True
