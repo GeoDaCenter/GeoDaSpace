183a3540ecaf7989c1a0c6c6528d01fb54bdcd88
stephens <stephens@a2f3893c-e248-4646-8fd3-e29ca8a5c454>
2013-07-03 22:21:33 +0000

    version bump

--- geodaspace/version.py
+++ geodaspace/version.py
@@ -51,7 +51,9 @@ def get_long_version():
 # version = "0.8.0"  #spreg r904, pysal 1.6.0dev
 # version_type = "alpha"
 # version_date = datetime.date(2013, 4, 26)
-
-version = "0.8.1"  # spreg r918, pysal 1.6.0dev
+# version = "0.8.1"  # spreg r918, pysal 1.6.0dev
+# version_type = "alpha"
+# version_date = datetime.date(2013, 5, 29)
+version = "0.8.2"  # spreg r970, pysal 1.6.0dev
 version_type = "alpha"
-version_date = datetime.date(2013, 5, 29)
+version_date = datetime.date(2013, 7, 3)
