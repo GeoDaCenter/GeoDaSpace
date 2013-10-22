28d1345ee722862b4fd6a587bc928f1d49753b4e
stephens <stephens@a2f3893c-e248-4646-8fd3-e29ca8a5c454>
2013-10-22 21:06:42 +0000

    bumping version for build

--- trunk/geodaspace/version.py
+++ trunk/geodaspace/version.py
@@ -60,6 +60,13 @@ def get_long_version():
 # version = "0.8.3"  # spreg r978, pysal 1.6.0dev
 # version_type = "alpha"
 # version_date = datetime.date(2013, 7, 29)
-version = "0.8.4"  # spreg r984, pysal 1.7.0dev
+# version = "0.8.4"  # spreg r984, pysal 1.7.0dev
+# version_type = "alpha"
+# version_date = datetime.date(2013, 8, 6)
+# version = "0.8.5"  # spreg r1014, pysal 1.7.0dev
+# version_type = "alpha"
+# version_date = datetime.date(2013, 10, 2)
+
+version = "0.8.6"  # spreg r1015, pysal 1.7.0dev
 version_type = "alpha"
-version_date = datetime.date(2013, 8, 6)
+version_date = datetime.date(2013, 10, 22)
