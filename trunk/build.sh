d49ca2a29bc65fbb7d682f0bfec9d223ebfce8e7
stephens <stephens@a2f3893c-e248-4646-8fd3-e29ca8a5c454>
2013-11-01 18:54:22 +0000

    Build modifications

--- trunk/build.sh
+++ trunk/build.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-
 PYTHONPATH=/Users/gspace/Desktop/pysal:/Users/gspace/Desktop/spreg/trunk
 export PYTHONPATH
-echo $PYTHONPATH
+#echo $PYTHONPATH
 cd /Users/gspace/Desktop/pysal; /usr/local/git/bin/git fetch
 
 cd /Users/gspace/Desktop/spreg/trunk
-rm -rf dist/
-rm -rf build/
-find . -name "*.pyc" -exec rm '{}' ';'
 svn update
 
-/Library/Frameworks/Python.framework/Versions/2.7/bin/python  build_geodaspace.py force
+/Library/Frameworks/Python.framework/Versions/2.7/bin/python  build_geodaspace.py 
 cd /Users/gspace/Desktop/spreg/trunk/dist/
 tar -czvf GeoDaSpace_OSX_Nightly.tar.gz GeoDaSpace\ OSX\ 0.8.6.app/
-cp GeoDaSpace_OSX_Nightly.tar.gz /Volumes/GeoDa/Projects/GeoDaSpace/Testing/
+cp GeoDaSpace_OSX_Nightly.tar.gz /Volumes/GeoDa/Projects/GeoDaSpace/Nightly/
 
+
+Revision: 1025
+Revision: 1025
+Revision: 1025
