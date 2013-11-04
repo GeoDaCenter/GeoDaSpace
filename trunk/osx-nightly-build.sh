2f876c4fc40b1d1bbff79a459a72faf34d6e594f
stephens <stephens@a2f3893c-e248-4646-8fd3-e29ca8a5c454>
2013-11-04 22:29:49 +0000

    build scripts

--- trunk/osx-nightly-build.sh
+++ trunk/osx-nightly-build.sh
@@ -4,12 +4,16 @@ export PYTHONPATH
 PATH=/usr/local/git/bin:/Library/Frameworks/Python.framework/Versions/2.7/bin:$PATH
 export PATH
 
+rm /Volumes/GeoDa/Projects/GeoDaSpace/Nightly/GeoDaSpace_OSX_Nightly.tar.gz
 
 cd ~/Desktop/pysal
 git fetch
 
 cd ~/Desktop/spreg/trunk
+/usr/bin/make clean
 svn update
+#svnversion >> ~/Desktop/spreg/trunk/rev.txt
+svn info | grep 'Revision' >> ~/Desktop/spreg/trunk/rev.txt
 
 python  build_geodaspace.py 
 cd ~/Desktop/spreg/trunk/dist/
