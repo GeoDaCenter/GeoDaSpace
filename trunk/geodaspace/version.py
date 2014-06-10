baa822dc7768cd14047f441806dae7a797aaa1bf
stephens <stephens@a2f3893c-e248-4646-8fd3-e29ca8a5c454>
2014-06-10 22:50:17 +0000

    Simplified versioning script, removing old build script, and adding pyinstaller spec script for osx.

--- trunk/geodaspace/version.py
+++ trunk/geodaspace/version.py
@@ -1,36 +1,15 @@
 import datetime
 
-# version_date = datetime.date.today()  # doing this causes the date string in the gui to update every day it is run!
-# Current Release. Note: when you bump, change srcfolder in osx-nightly-build script
-version = "1.0"  # spreg r1083, pysal 1.7.0dev
-
-# toggle these 2 below for official alpha releases
-version_type = "alpha"
-#version_type = 'nightly'
+version = "1.0"
 
 def get_long_version():
     s = ""
-    if version_type == 'nightly':
-        s += "(nightly) "
-        s += " | "
-        s += "spReg revision "
-        s += rev
-        s += " | "
-        version_date = datetime.date.today()
-        s += version_date.strftime('%B %d, %Y')
-        s += " | "
-    if version_type == 'alpha':
-        s += "(alpha) "
-        s += " | "
-        version_date = datetime.date(2014, 6, 1)
-        s += version_date.strftime('%B %d, %Y')
-        s += " | "
-    elif version_type == 'beta':
-        s += "(beta) "
-    s += "Release "
+    #s += " | "
+    #version_date = datetime.date.today()
+    #s += version_date.strftime('%B %d, %Y')
+    #s += " | "
+    s += "version  "
     s += version
     return s
 
-# Version History
 
-rev = '1112'
