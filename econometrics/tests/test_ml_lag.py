6a72249aa97906063adafb7de7bcca631c7c069f
stephens <stephens@a2f3893c-e248-4646-8fd3-e29ca8a5c454>
2014-07-12 17:22:29 +0000

    Employing the unittest skip decorator so that tests are skipped if a condition is not satisified. In this case, if scipy version is less than 12, this unittest class is run but failing tests are not counted as Failures once this gets downstream to pysal.

--- econometrics/tests/test_ml_lag.py
+++ econometrics/tests/test_ml_lag.py
@@ -1,9 +1,12 @@
 import unittest
 import pysal
+import scipy
 import numpy as np
 from econometrics.ml_lag import ML_Lag
 from econometrics import utils
 
+@unittest.skipIf(int(scipy.__version__.split(".")[1]) < 12,
+        "Max Likelihood requires SciPy version 12 or newer.")
 class TestMLError(unittest.TestCase):
     def setUp(self):
         db =  pysal.open(pysal.examples.get_path("baltim.dbf"),'r')
